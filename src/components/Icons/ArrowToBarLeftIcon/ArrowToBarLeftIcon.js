519da8c429939febc1901aefeeb8bad49d2c4d96
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-12-11 17:44:02 -0500

    Create ArrowToBarLeftIcon.js

---
+++ src/components/Icons/ArrowToBarLeftIcon/ArrowToBarLeftIcon.js
@@ -0,0 +1,8 @@
+const ArrowToBarLeftIcon = () => {
+  return (
+    <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="arrow to bar left icon">
+      <path d="M4 4V20M8 12H20M8 12L12 8M8 12L12 16" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  )
+}
+export default ArrowToBarLeftIcon
