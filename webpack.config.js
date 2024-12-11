472817e1430fac5056354c7860f82673368ef51f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-12-11 11:35:39 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(process.cwd(), 'build'),
-    publicPath: '/',
+    publicPath: '/devour-clone/',
   },
     
   module: {
