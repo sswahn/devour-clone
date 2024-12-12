c488dbc519946cb5d75fba4dc0eff72fce07a8c2
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-12-12 10:27:29 -0500

    Update SideBar.js

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -26,7 +26,7 @@ const UserIcon = ({ size }) => {
 
 const TestAvatar = ({ className, image, username, onClick, size }) => {
   return (
-    <div className={`${styles.avatar} ${className || ''}`} onClick={onClick} aria-label={`${username}'s avatar`}>
+    <div className={`${className || ''}`} onClick={onClick} aria-label={`${username}'s avatar`}>
       {image 
         ? <img src={image} alt={`${username}'s avatar`} width={size || '40px'} height={size || '40px'} />
         : <UserIcon size={size} />
