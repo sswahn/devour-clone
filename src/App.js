a0450007e3335f407fdb5d7bdb648a57b11d7052
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-13 16:12:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react'
 import ErrorBoundary from './ErrorBoundary'
 import Provider from './Provider'
 import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner'
 const SideBar = lazy(() => import('./components/SideBar/SideBar'))
 import './index.css'
@@ -11,6 +12,7 @@ export default () =>
     <ErrorBoundary>
       <Provider>
         <Header />
+        <Main />
         <Suspense fallback={<LoadingSpinner />}>
           <SideBar />
         </Suspense>
