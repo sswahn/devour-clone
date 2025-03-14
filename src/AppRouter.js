eca8a9ab5be32a7fb3e8cc80255009e679b210e2
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-14 15:34:07 -0400

    Update AppRouter.js

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,1 +1,17 @@
-// blah
+import Router from './components/Router/Router'
+import Route from './components/Router/Route'
+import Main from './components/Main/Main'
+import Camera from './components/Camera/Camera'
+import NotFound from './components/NotFound/NotFound'
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Route path="/" component={Main} />
+      <Route path="/camera" component={Camera} />
+      <Route component={NotFound}
+    </Router>
+  )
+}
+
+export default AppRouter
