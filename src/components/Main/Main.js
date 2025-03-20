ae6c1667a1ff568bf8cf03ba4140504cdf98f734
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-20 09:10:00 -0400

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,22 +1,46 @@
-import { useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { Context } from '../../Provider'
 import Modal from '../Modal/Modal'
+import database from '@sswahn/database'
 
 const Main = () => {
   const [context, dispatch] = useContext(Context)
+  const [data, setData] = useState([])
+  const db = database()
 
+  const loadData = async () => {
+    const images = await db.get('images')
+    const video = await db.get('video')
+
+    console.log('images: ', images)
+    console.log('video: ', video)
+    
+    setData([{
+      id: 1,
+      username: 'sswahn',
+      location: 'Chiang Mai',
+      images,
+      video
+    }])
+  }
+  
   const handleCloseModal = event => {
     dispatch({ type: 'modal', payload: { isOpen: false, content: <></> } })
   }
-  
+
+  useEffect(() => {
+    loadData()
+  }, [])
+      
   return (
     <main>
-      <div className="card" aria-label="">  {/* break card out into its own component and reuse */}
-        <div className="card-header"></div>
-        <div className="card-content"></div>
-        <div className="card-actions"></div>
-      </div>
-      
+      {data.length && data.map(item =>
+        <div key={item.id} className="card" aria-label="">
+          <div className="card-header">{/* username, location */}</div>
+          <div className="card-content">{item.image}</div>
+          <div className="card-actions"></div>
+        </div>
+      }
       <Modal className="camera-modal" open={context.modal.isOpen} onClose={handleCloseModal}>
         {context.modal.content}
       </Modal>
