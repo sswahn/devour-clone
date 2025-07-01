1ac95d6f4d3c168e1657b486da198eac52e1acb4
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-07-01 07:11:00 -0400

    Update Dropdown.js

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import styles from './styles.module.css'
 
-const Dropdown = ({ className, icon: ButtonIcon, text, options }) => {
+const Dropdown = ({ className, icon: ButtonIcon, text = '', options }) => {
   const [isOpen, setIsOpen] = useState(false)
   const dropdownRef = useRef(null)
   const menuRef = useRef(null)
@@ -27,7 +27,7 @@ const Dropdown = ({ className, icon: ButtonIcon, text, options }) => {
     <div className={`${styles.dropdown} ${className || ''}`}>
       <button type="button" onClick={toggleDropdown} ref={dropdownRef} aria-label="dropdown button" aria-haspopup="true" aria-expanded={isOpen}>
         {ButtonIcon && <div className={styles.icon}><ButtonIcon /></div>} 
-        {<div>{text}</div>}
+        {text && <div>{text}</div>}
       </button>
       <menu className={isOpen ? styles.open : styles.closed} ref={menuRef} aria-hidden={!isOpen}>
         {options.map(({ icon: Icon, label, onClick }, index) => (
