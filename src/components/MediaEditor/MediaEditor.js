3a4ecc67e673df8d64a00c7360c3af61ee938017
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-07-03 08:45:38 -0400

    Update MediaEditor.js

--- src/components/MediaEditor/MediaEditor.js
+++ src/components/MediaEditor/MediaEditor.js
@@ -301,11 +301,11 @@ const MediaEditor = memo(({ index, type, mediaEditorRef }) => {
           </button>
           <div>
             <input id="saturation" type="range" min="5" max="100" value={saturate} onChange={handleFilterSaturate} />
-            <label htmlFor="saturation"> {saturate}% Saturation</label>
+            <label htmlFor="saturation">{saturate}% Saturation</label>
           </div>
           <div>
             <input id="brightness" type="range" min="20" max="200" value={brightness} onChange={handleFilterBrightness} />
-            <label htmlFor="brightness"> {brightness}% Brightness</label>
+            <label htmlFor="brightness">{brightness}% Brightness</label>
           </div>
         </div>
         
