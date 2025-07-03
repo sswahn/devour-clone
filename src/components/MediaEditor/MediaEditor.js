86458782e349a6322b541e2aab56e9da234878e6
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-07-03 08:24:34 -0400

    Update MediaEditor.js

--- src/components/MediaEditor/MediaEditor.js
+++ src/components/MediaEditor/MediaEditor.js
@@ -82,8 +82,10 @@ const MediaEditor = memo(({ index, type, mediaEditorRef }) => {
   useEffect(() => {
     const filters = storage.local.get('image_editor_styles')[0].filter
     if (filters.includes('saturate')) {
-      const value = filters.split('saturate(').at(-1).split(')').at(0).slice(0, -2)
+      const value = filters.split('saturate(').at(-1).slice(0, -2)
+      
       console.log('saturate value: ', value)
+      
       setBrightness(
        value 
       )
