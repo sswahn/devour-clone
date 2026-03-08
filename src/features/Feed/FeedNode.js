fc1109f5fd83cd8dae6bcaf115baf641761d53a1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-08 16:10:42 -0400

    Display video in FeedNode component

--- src/features/Feed/FeedNode.js
+++ src/features/Feed/FeedNode.js
@@ -4,7 +4,9 @@ import styles from './feed.module.css'
 function FeedNode({ item }) {
 
   return (
-    <div className={styles.feedNode}>{item}</div>
+    <div className={styles.feedNode}>
+      <video src={item.video} preload="metadata" muted playsInline loop>
+    </div>
   )
 }
 
