0fa503967e2232121cbb4bcd1a113fb463a84b7b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-08 16:23:07 -0400

    Add ref to video element in FeedNode component

--- src/features/Feed/FeedNode.js
+++ src/features/Feed/FeedNode.js
@@ -1,11 +1,12 @@
-
+import { useRef }
 import styles from './feed.module.css'
 
 function FeedNode({ item }) {
-
+  const ref = useRef()
+  
   return (
     <div className={styles.feedNode}>
-      <video src={item.video} preload="metadata" muted playsInline loop />
+      <video ref={ref} src={item.video} preload="metadata" muted playsInline loop />
     </div>
   )
 }
