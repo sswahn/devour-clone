242016ce5bc849ff15fd13c6edbd9df1e22d6913
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-09 10:39:28 -0400

    Update LikeButton.js

--- src/features/Feed/LikeButton.js
+++ src/features/Feed/LikeButton.js
@@ -1,13 +1,17 @@
 import { useState } from 'react'
+// import icons
 
 function LikeButton() {
+  const [liked, setLiked] = useState(false)
 
   const onClick = event => {
     // handle like
   }
   
   return (
-    <></>
+    <button type="button" aria-label="Like" aria-pressed={liked}>
+      {liked ? 'liked icon' : 'like icon' }
+    </button>
   )
 }
 
