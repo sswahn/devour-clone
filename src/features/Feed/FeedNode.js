5ba0ff078267836d56fa469bd71d7612545e0359
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-09 10:31:53 -0400

    Add tabIndex and like button to FeedNode component

--- src/features/Feed/FeedNode.js
+++ src/features/Feed/FeedNode.js
@@ -5,13 +5,16 @@ function FeedNode({ item, index, count }) {
   const ref = useRef()
   
   return (
-    <article className={styles.feedNode} aria-posinset={index} aria-setsize={count}>
+    <article className={styles.feedNode} tabIndex={index} aria-posinset={index} aria-setsize={count}>
       <header>{/* user icon name etc. maybe the location should be the title <h2>? */}</header>
         <figure>
           <video ref={ref} src={item.video} preload="metadata" muted playsInline loop />
           {item.caption ?? <figcaption>{item.caption}</figcaption>}
         </figure>
-      <footer>{/* likes comments etc. */}</footer>
+      <footer>
+        {/* likes, comments, views etc. */}
+        <button type="button" aria-label="Like" aria-pressed={false}>like</button>
+      </footer>
     </article>
   )
 }
