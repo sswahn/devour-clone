6acff70e33b0fa663bd507ac0a254455973c9770
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-09 09:43:41 -0400

    Enhance FeedNode component with index and count props

--- src/features/Feed/FeedNode.js
+++ src/features/Feed/FeedNode.js
@@ -1,13 +1,18 @@
 import { useRef } from 'react'
 import styles from './feed.module.css'
 
-function FeedNode({ item }) {
+function FeedNode({ item, index, count }) {
   const ref = useRef()
   
   return (
-    <div className={styles.feedNode}>
-      <video ref={ref} src={item.video} preload="metadata" muted playsInline loop />
-    </div>
+    <article className={styles.feedNode} aria-posinset={index} aria-setsize={count}>
+      <header>{/* user icon name etc. */}</header>
+        <figure>
+          <video ref={ref} src={item.video} preload="metadata" muted playsInline loop />
+          {item.caption ?? <figcaption>{item.caption}</figcaption>}
+        </figure>
+      <footer>{/* likes comments etc. */}</footer>
+    </article>
   )
 }
 
