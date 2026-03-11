a7c29556e875b1431a2ef17493e96874c021638b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-11 10:48:21 -0400

    Update NavOverlay.js

--- src/features/Camera/NavOverlay.js
+++ src/features/Camera/NavOverlay.js
@@ -1,10 +1,6 @@
-
 import { useContext } from 'react'
 import { Context } from '../../Provider'
-import CameraIcon from '../Icons/CameraIcon/CameraIcon'
 import FileVideoIcon from '../Icons/FileVideoIcon/FileVideoIcon'
-import ImageIcon from '../Icons/ImageIcon/ImageIcon'
-import VideoIcon from '../Icons/VideoIcon/VideoIcon'
 import LocationIcon from '../Icons/LocationIcon/LocationIcon'
 import MicrophoneIcon from '../Icons/MicrophoneIcon/MicrophoneIcon'
 import MicrophoneSlashIcon from '../Icons/MicrophoneIcon/MicrophoneSlashIcon'
@@ -16,36 +12,17 @@ const NavOverlay = ({ timer, previewFiles, openSubmit, toggleMute, mute }) => {
     dispatch({ type: 'field', payload: context.field === 'location' ? undefined : 'location' })
   }
   
-  const handleModeCamera = event => {
-    dispatch({ type: 'mode', payload: 'camera' })
-  }
-  
-  const handleModeVideo = event => {
-    dispatch({ type: 'mode', payload: 'video' })
-  }
-  
-  const adjustHeight = () => {
-    if (context.images.length && context.video.length) {
-      return '415px'
-    }
-    if (context.images.length || context.video.length) {
-      return '333px'
-    }
-    return '250px'
-  }
-  
-  
   function blobsToBase64(blobs) {
     return Promise.all(blobs.map(blob => {
       return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(blob);
+        const reader = new FileReader()
+        reader.readAsDataURL(blob)
         reader.onloadend = function() {
-          resolve(reader.result);
-        };
-        reader.onerror = reject;
-      });
-    }));
+          resolve(reader.result)
+        }
+        reader.onerror = reject
+      })
+    }))
   }
   
   const handleSubmitTest = async event => {
@@ -71,7 +48,7 @@ const NavOverlay = ({ timer, previewFiles, openSubmit, toggleMute, mute }) => {
   
   return (
     <>
-      <div className="camera-navigation" style={{ height: adjustHeight() }}>
+      <div className="camera-navigation">
       
       {/*
         <button id="swap-segment" onClick={handleSubmitTest} type="button">
@@ -79,13 +56,6 @@ const NavOverlay = ({ timer, previewFiles, openSubmit, toggleMute, mute }) => {
         </button>
       */}
       
-        {!!context.images.length && (
-          <button id="images" className="icon-btn" onClick={previewFiles} type="button" aria-label="preview and edit images" aria-haspopup="dialog">
-            <ImageIcon />
-            <div className="badge" role="status" aria-label="video count">{context.images.length}</div>
-            <div className="tooltip" role="tooltip">Images</div>
-          </button>
-        )}
         {!!context.video.length && (
           <button id="video" className="icon-btn" onClick={previewFiles} type="button" aria-label="preview and edit video" aria-haspopup="dialog">
             <FileVideoIcon />
@@ -93,25 +63,7 @@ const NavOverlay = ({ timer, previewFiles, openSubmit, toggleMute, mute }) => {
             <div className="tooltip" role="tooltip">Video</div>
           </button>
         )}
-        <button 
-          className="icon-btn"
-          onClick={handleModeCamera} 
-          type="button"
-          aria-label="camera mode"
-          aria-selected={context.mode === 'camera'}>
-          <CameraIcon style={{ fill: context.mode === 'camera' ? 'white' : 'gray' }} />
-          <div className="tooltip" role="tooltip">Camera</div>
-        </button>
-        <button 
-          className="icon-btn"
-          onClick={handleModeVideo} 
-          type="button"
-          aria-label="video mode"
-          aria-selected={context.mode === 'video' || context.mode === 'recording'}
-          aria-controls="mute-video">
-          <VideoIcon style={{ fill: context.mode === 'video' || context.mode === 'recording' ? 'white' : 'gray' }} />
-          <div className="tooltip" role="tooltip">Video</div>
-        </button>
+            
         <button className="icon-btn" onClick={openSubmit} type="button" aria-label="add your location" aria-haspopup="dialog">
           <LocationIcon />
           <div className="tooltip" role="tooltip">Location</div>
@@ -123,22 +75,20 @@ const NavOverlay = ({ timer, previewFiles, openSubmit, toggleMute, mute }) => {
           {`${Math.floor(timer / 60)}:${String(timer % 60).padStart(2, "0")}`}
         </div>
       )}
-      
-      {(context.mode === 'video' || context.mode === 'recording') && (
-        <div id="mute-video" className="video-mute" aria-hidden="false">
-          {mute ? (
-            <button className="icon-btn" onClick={toggleMute} type="button" aria-label="unmute video">
-              <MicrophoneSlashIcon />
-              <div className="tooltip" role="tooltip">Unmute</div>
-            </button>
-          ) : (
-            <button className="icon-btn" onClick={toggleMute} type="button" aria-label="mute video">
-              <MicrophoneIcon />
-              <div className="tooltip" role="tooltip">Mute</div>
-            </button>
-          )}
-        </div>
-      )}
+     
+      <div id="mute-video" className="video-mute" aria-hidden="false">
+        {mute ? (
+          <button className="icon-btn" onClick={toggleMute} type="button" aria-label="unmute video">
+            <MicrophoneSlashIcon />
+            <div className="tooltip" role="tooltip">Unmute</div>
+          </button>
+        ) : (
+          <button className="icon-btn" onClick={toggleMute} type="button" aria-label="mute video">
+            <MicrophoneIcon />
+            <div className="tooltip" role="tooltip">Mute</div>
+          </button>
+        )}
+      </div>
     </>
   )
 }
