3f7d7e8ebbbc8f5b67a14fd13808a480a720a831
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-12 13:43:34 -0400

    Update LocationButton.js

--- src/features/Camera/LocationButton.js
+++ src/features/Camera/LocationButton.js
@@ -2,9 +2,12 @@ import LocationIcon from '../Icons/LocationIcon/LocationIcon'
 
 function LocationButton() {
 
-
+  const handleOpenLocation = event => {
+    
+  }
+  
   return (
-    <button className="icon-btn" onClick={openSubmit} type="button" aria-label="add your location" aria-haspopup="dialog>
+    <button className="icon-btn" onClick={handleOpenLocation} type="button" aria-label="add your location" aria-haspopup="dialog>
       <LocationIcon />
       <div className="tooltip" role="tooltip">Location</div>
     </button>
