a56722929201b424f12145e1f8eb933621deb43d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-12 13:44:46 -0400

    Update LocationButton.js

--- src/features/Camera/LocationButton.js
+++ src/features/Camera/LocationButton.js
@@ -5,6 +5,8 @@ function LocationButton() {
   const handleOpenLocation = event => {
     
   }
+
+  // onHover tool tips dont work on touch screen. consider a fix.
   
   return (
     <button className="icon-btn" onClick={handleOpenLocation} type="button" aria-label="add your location" aria-haspopup="dialog>
