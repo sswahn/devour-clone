f5125096a0e18c65ec8686bf8eb272a05c4ba90f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-12 11:41:16 -0400

    Update MuteButton.js

--- src/features/Camera/MuteButton.js
+++ src/features/Camera/MuteButton.js
@@ -9,19 +9,12 @@ function MuteButton({ streamRef }) {
   }
   
   return (
-      <div id="mute-video" className="video-mute" aria-hidden="false">
-        {mute ? (
-          <button className="icon-btn" onClick={toggleMute} type="button" aria-label="unmute video">
-            <MicrophoneSlashIcon />
-            <div className="tooltip" role="tooltip">Unmute</div>
-          </button>
-        ) : (
-          <button className="icon-btn" onClick={toggleMute} type="button" aria-label="mute video">
-            <MicrophoneIcon />
-            <div className="tooltip" role="tooltip">Mute</div>
-          </button>
-        )}
-      </div>
+    <div id="mute-video" className="video-mute" aria-hidden="false">
+      <button className="icon-btn" onClick={toggleMute} type="button" aria-label="unmute video">
+        {mute ? <MicrophoneSlashIcon /> : <MicrophoneIcon />}
+        <div className="tooltip" role="tooltip">{mute ? 'Unmute' : 'Mute'</div>
+      </button>
+    </div>
   )
 }
 
