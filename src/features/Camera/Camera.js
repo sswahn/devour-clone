347ea0c4b3e95788dc2ef2710c6120e475dc674d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-12 16:08:36 -0400

    Update Camera.js

--- src/features/Camera/Camera.js
+++ src/features/Camera/Camera.js
@@ -1,31 +1,14 @@
-import { useState, useContext } from 'react'
-import { Context } from '../../Provider'
-import camera from '../../utilities/camera'
-import storage from '@sswahn/storage'
-import database from '@sswahn/database'
 
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
+import ViewPort from './ViewPort'
 import NavOverlay from './NavOverlay'
 
 const Camera = () => {
-  const [context, dispatch] = useContext(Context)
-
-  const [isFlashing, setIsFlashing] = useState(false)
-  
-  
-
-
-  // move the rest of this into componet into NavOverlay and make NavOverlay into Camera
-  
+ 
   return (
-    <div className="card" aria-label="camera viewport">
-      <div className={`flash-overlay ${isFlashing ? 'flash' : ''}`}></div>
-        <>
-          <div className="card-content">
-            <video ref={videoRef} className="camera" autoPlay muted playsInline aria-label="camera feed" aria-live="assertive"></video>
-            <NavOverlay />
-          </div>
-        </>
+    <div className="card" aria-label="camera">
+      <ViewPort />
+      <NavOverlay />
     </div>
   )
 }
