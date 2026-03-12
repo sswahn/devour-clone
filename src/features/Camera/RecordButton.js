2f3941fe65e421b6747d2ae55b7617bf4acea224
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-12 11:46:12 -0400

    Update RecordButton.js

--- src/features/Camera/RecordButton.js
+++ src/features/Camera/RecordButton.js
@@ -3,7 +3,19 @@ import { useState } from 'react'
 function RecordButton() {
 
   return (
-    <></>
+    <div className="card-actions">
+      <div className="camera-button-container">
+        <button 
+          className="camera-button" 
+          onClick={handleCameraButton} 
+          type="button" 
+          aria-label="camera button"
+          style={{
+            backgroundColor: context.mode === 'recording' ? '#cb4154' : '#e5e4e2', 
+            borderColor: context.mode === 'recording' ? '#eb4c42' : 'white'
+        }}></button>
+      </div>
+    </div>
   )
 }
 
