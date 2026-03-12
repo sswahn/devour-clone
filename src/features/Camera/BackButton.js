cb813e9b54f11bb2a27fae0c1fa6c14d14de64eb
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-12 12:12:53 -0400

    Update and rename Back button.js to BackButton.js

--- src/features/Camera/BackButton.js
+++ src/features/Camera/BackButton.js
@@ -2,6 +2,12 @@
 
 function BackButton() {
   
+  const handleCloseCamera = event => {
+    stopCamera()
+    
+    // this is no longer a modal so remove this type of dispatch
+    dispatch({ type: 'modal', payload: { isOpen: false, content: <></> } })
+  }
 
   return (
     <button className="camera-back-btn" onClick={handleCloseCamera} type="button" aria-label="close camera button">
