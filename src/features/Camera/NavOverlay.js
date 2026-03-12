c5ceb66722d3122ce770a4f852cc3380293440a6
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-12 14:17:00 -0400

    Update NavOverlay.js

--- src/features/Camera/NavOverlay.js
+++ src/features/Camera/NavOverlay.js
@@ -12,48 +12,13 @@ import LocationButton from './LocationButton'
 const NavOverlay = ({ openSubmit }) => {
   const [context, dispatch] = useContext(Context)
   
-  function blobsToBase64(blobs) {
-    return Promise.all(blobs.map(blob => {
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader()
-        reader.readAsDataURL(blob)
-        reader.onloadend = function() {
-          resolve(reader.result)
-        }
-        reader.onerror = reject
-      })
-    }))
-  }
-  
-  const handleSubmitTest = async event => {
-    const base64Arr = await blobsToBase64(context.video)
-    const request = {
-      video: base64Arr
-    }
-    
-    const response = await fetch('/api/v1/test', {
-      method: 'post',
-      body: JSON.stringify(request),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    
-    console.log('response: ', response)
-    
-    const json = await response.json()
-    
-    console.log('json: ', json)
-  }
   
   return (
     <section className="camera-navigation">
-      <header className="card-header">
-        <BackButton stopCamera={stopCamera} />
-        <RecordTimer />
-        <LightButton streamRef={streamRef} />
-      </header>
-    
+      <BackButton stopCamera={stopCamera} />
+      <RecordTimer />
+      <LightButton streamRef={streamRef} />
+      
       <MuteButton streamRef={streamRef} />
       <RecordButton />
       <LocationButton />
