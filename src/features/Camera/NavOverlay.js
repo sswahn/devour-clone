d66f25485ac4bba0b239641d3bd3df789240aa21
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-12 14:17:55 -0400

    Update NavOverlay.js

--- src/features/Camera/NavOverlay.js
+++ src/features/Camera/NavOverlay.js
@@ -11,7 +11,26 @@ import LocationButton from './LocationButton'
 
 const NavOverlay = ({ openSubmit }) => {
   const [context, dispatch] = useContext(Context)
+  const streamRef = useRef(null)
+  const videoRef = useRef(null)
+  const db = database()
+
+  const startCamera = async () => {
+    try {
+      const stream = await camera.on()
+      streamRef.current = stream
+      videoRef.current.srcObject = stream
+    } catch (error) {
+      console.error('Error accessing camera.')
+    }
+  }
   
+  const stopCamera = event => {
+    if (streamRef.current) {
+      camera.off(streamRef.current) 
+      streamRef.current = null
+    }
+  }
   
   return (
     <section className="camera-navigation">
