2f7b8b4726b9c54f0f68cc9dc830eb909103c237
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-13 16:21:57 -0400

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,5 @@
 import { useState, useContext, useEffect, Suspense, lazy } from 'react'
 import { Context } from '../../Provider'
-import database from '@sswahn/database'
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
 import Portal from './Portal/Portal'
 const Camera = lazy(() => import('./features/Camera/Camera')
