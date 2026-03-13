d920ded520be307cbaf28fc39012ddff946a3736
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-13 17:39:40 -0400

    Update Feed.js

--- src/features/Feed/Feed.js
+++ src/features/Feed/Feed.js
@@ -1,14 +1,21 @@
 import { useState } from 'react'
+import server from '../../utilities/server'
 import FeedNode from './FeedNode'
 import Sentinel from './Sentinel' // sentinel triggers infinite loading
 import styles from './feed.module.css'
 
 function Feed() {
   const [data, setData] = useState([])
+  const [batch, setBatch] = useState(0)
   const [loading, setLoading] = useState(false)
 
-  const loadMoreData = () => {
-    // fetch next batch from server
+  const loadMoreData = async event => {
+    const response = await server.get(`${config.api.feed}/${bach}`)
+    if (!response.error) {
+      return alert(response.error)
+    }
+    
+    setData({ ...data, ...response.message })
   }
   
   return (
