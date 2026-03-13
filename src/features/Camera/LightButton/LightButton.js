5a2b5aaf8cd3978658925a5989d89bd1dba6c795
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-13 11:33:04 -0400

    Update and rename src/features/Camera/LightButton.js to src/features/Camera/LightButton/LightButton.js

--- src/features/Camera/LightButton/LightButton.js
+++ src/features/Camera/LightButton/LightButton.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import SunIcon from '../Icons/SunIcon/SunIcon'
 import DarkSunIcon from '../Icons/SunIcon/DarkSunIcon'
+import styles from './lightbutton.module.css'
 
 function LightButton({ streamRef }) {
   const [light, setLight] = useState(false)
@@ -11,7 +12,7 @@ function LightButton({ streamRef }) {
   }
   
   return (
-    <button className="camera-light-btn" onClick={handleTurnOnLight} type="button" aria-label="camera light button">
+    <button className={styles.lightButton} onClick={handleTurnOnLight} type="button" aria-label="camera light button">
       {light ? <DarkSunIcon /> : <SunIcon />}
       <div className="tooltip" role="tooltip">Light</div>
     </button>
