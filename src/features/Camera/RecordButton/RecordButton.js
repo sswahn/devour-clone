6e9687e6a29d22f3040f07206b89683137a4569d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-13 13:27:40 -0400

    Update RecordButton.js

--- src/features/Camera/RecordButton/RecordButton.js
+++ src/features/Camera/RecordButton/RecordButton.js
@@ -10,35 +10,37 @@ function RecordButton({ streamRef }) {
   const db = database()
   
   const handleRecordVideo = () => {
-    // check remaining time?
-    dispatch({ type: 'mode', payload: 'recording' })
+    // check remaining time
+    dispatch({ type: 'recording', payload: true })
     const recorder = camera.startRecording(streamRef.current, framesRef.current)
     recorderRef.current = recorder
   }
 
   const handleStopRecordVideo = async () => {
-    
-    dispatch({ type: 'mode', payload: 'video' }) // change mode from video to recording true/false
+    dispatch({ type: 'recording', payload: false })
     
     timerRef.current = timer
+    
     const blob = await camera.stopRecording(recorderRef.current, framesRef.current)
     const video = [ ...context.video, blob ]
     const currentDuration = context.video_duration.reduce((acc, val) => acc + val, 0)
     const duration = [ ...context.video_duration, 300 - timerRef.current - currentDuration ]
+    
     dispatch({ type: 'video_duration', payload: duration })
-    dispatch({ type: 'video', payload: video }) 
+    dispatch({ type: 'video', payload: video })
+    
     db.put({ id: 'video', video, duration })
   }
   
   const handleRecordButton = event => {
-    context.mode === 'recording' ? handleStopRecordVideo() : handleRecordVideo()
+    context.recording ? handleStopRecordVideo() : handleRecordVideo()
   }
   
   return (
     <div className={styles.recordButtonContainer}>
       <button className={styles.recordButton} onClick={handleRecordButton} type="button" aria-label="record button" style={{
-        backgroundColor: context.mode === 'recording' ? '#cb4154' : '#e5e4e2', 
-        borderColor: context.mode === 'recording' ? '#eb4c42' : 'white'
+        backgroundColor: context.recording ? '#cb4154' : '#e5e4e2', 
+        borderColor: context.recording ? '#eb4c42' : 'white'
       }}></button>
     </div>
   )
