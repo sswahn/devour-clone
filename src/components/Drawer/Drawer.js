731ea475715d557be45a79201a5b07c9522cd590
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-14 11:23:03 -0400

    Create Drawer.js

---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,33 @@
+import { useContext } from 'react'
+import { Context } from '../../Provider'
+
+function Drawer() {
+  const [context, dispatch] = useContext(Context)
+
+  const handleOpenProfile = event => {
+    
+  }
+
+  const handleOpenCamera = event => {
+    
+  }
+
+  const handleShowNotifications = event => {
+    
+  }
+
+  const handleLogout = event => {
+    
+  }
+  
+  return (
+    <nav>
+      <button type="button" onClick={handleOpenProfile}>Profile<button>
+      <button type="button" onClick={handleOpenCamera}>Camera<button>
+      <button type="button" onClick={handleShowNotifications}>Notifications<button>
+      <button type="button" onClick={handleLogout}>Logout<button>
+    </nav>
+  )
+}
+
+export default Drawer
