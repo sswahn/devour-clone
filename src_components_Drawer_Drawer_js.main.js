98f4df6c8159489183f15e71282dbb2076c191b8
sswahn <sswahn@users.noreply.github.com>
2026-03-14 18:28:21 +0000

    deploy: ea30d5e0d4e151e9a317cf61a2d6e25522defd45

---
+++ src_components_Drawer_Drawer_js.main.js
@@ -0,0 +1,32 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkdevour_clone"] = self["webpackChunkdevour_clone"] || []).push([["src_components_Drawer_Drawer_js"],{
+
+/***/ "./src/components/Drawer/Drawer.js"
+/*!*****************************************!*\
+  !*** ./src/components/Drawer/Drawer.js ***!
+  \*****************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _babel_runtime_helpers_slicedToArray__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/slicedToArray */ \"./node_modules/@babel/runtime/helpers/esm/slicedToArray.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _Provider__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../Provider */ \"./src/Provider.js\");\n/* harmony import */ var _drawer_module_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./drawer.module.css */ \"./src/components/Drawer/drawer.module.css\");\n/* provided dependency */ var React = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\n\n\n\nfunction Drawer() {\n  var _useContext = (0,react__WEBPACK_IMPORTED_MODULE_1__.useContext)(_Provider__WEBPACK_IMPORTED_MODULE_2__.Context),\n    _useContext2 = (0,_babel_runtime_helpers_slicedToArray__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(_useContext, 2),\n    context = _useContext2[0],\n    dispatch = _useContext2[1];\n  var handleOpenProfile = function handleOpenProfile(event) {};\n  var handleOpenCamera = function handleOpenCamera(event) {};\n  var handleShowNotifications = function handleShowNotifications(event) {};\n  var handleLogout = function handleLogout(event) {};\n  return /*#__PURE__*/React.createElement(\"nav\", {\n    className: _drawer_module_css__WEBPACK_IMPORTED_MODULE_3__[\"default\"].drawer\n  }, /*#__PURE__*/React.createElement(\"button\", {\n    type: \"button\",\n    onClick: handleOpenProfile\n  }, \"Profile\"), /*#__PURE__*/React.createElement(\"button\", {\n    type: \"button\",\n    onClick: handleOpenCamera\n  }, \"Camera\"), /*#__PURE__*/React.createElement(\"button\", {\n    type: \"button\",\n    onClick: handleShowNotifications\n  }, \"Notifications\"), /*#__PURE__*/React.createElement(\"button\", {\n    type: \"button\",\n    onClick: handleLogout\n  }, \"Logout\"));\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Drawer);\n\n//# sourceURL=webpack://devour-clone/./src/components/Drawer/Drawer.js?\n}");
+
+/***/ },
+
+/***/ "./src/components/Drawer/drawer.module.css"
+/*!*************************************************!*\
+  !*** ./src/components/Drawer/drawer.module.css ***!
+  \*************************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n// extracted by mini-css-extract-plugin\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({\"drawer\":\"g4TFtCgLYfF1xSo0uGH4\",\"drawer-overlay\":\"l7CHW7dtXPfpkrV47YxS\",\"drawer-slide\":\"lXY_AhQgaIWaWW0uKgZ2\",\"drawer-nav\":\"_3ayFbvwBsDY7hQnbIs5\"});\n\n//# sourceURL=webpack://devour-clone/./src/components/Drawer/drawer.module.css?\n}");
+
+/***/ }
+
+}]);
