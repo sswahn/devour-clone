799a76a2e164efeae7f415aa0f87d6d9b207f645
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-14 13:21:36 -0400

    Update Feed.js

--- src/features/Feed/Feed.js
+++ src/features/Feed/Feed.js
@@ -21,10 +21,12 @@ function Feed() {
   
   return (
     <section className={styles.feed} role="feed" aria-busy={loading}>
+{/*
       {data.map((item, index) => {
         <FeedNode key={item.id} item={item} index={idex + 1} count={items.length} />
       })}
       <Sentinel onVisible={loadMoreData} />
+*/}
     </section>
   )
 }
