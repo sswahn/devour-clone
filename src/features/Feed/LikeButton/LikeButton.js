8c996ede03c76553fde5952c1d497c31a97f6e05
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-14 12:48:25 -0400

    Update LikeButton.js

--- src/features/Feed/LikeButton/LikeButton.js
+++ src/features/Feed/LikeButton/LikeButton.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import server from '../../../utilitles/server'
+import server from '../../../utilities/server'
 import config from '../../../config'
 // import icons
 
