500c391fb65dc1918efd20b6f5bd2093f0dfe1f7
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-16 17:13:03 -0400

    Update Camera.js

--- src/features/Camera/Camera.js
+++ src/features/Camera/Camera.js
@@ -46,10 +46,11 @@ function Camera() {
       <BackButton stopCamera={stopCamera} />
       <RecordTimer timer={timer} setTimer={setTimer} />
       <LightButton streamRef={streamRef} />
-      <MuteButton streamRef={streamRef} />
+      
      
-    {/*    <RecordButton streamRef={streamRef} timer={timer} />
+    {/*    <MuteButton streamRef={streamRef} />
     */}
+      <RecordButton streamRef={streamRef} timer={timer} />
       <LocationButton /> 
       <ViewPort videoRef={videoRef} />
     </section>
