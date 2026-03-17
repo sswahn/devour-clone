9af1ce8aed9cbf01dbd17a5a92e7457969181c02
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-17 10:55:35 -0400

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -104,7 +104,7 @@ const Sidebar = () => {
   
   return (
     <>
-  {isOpen &&
+  {isOpen && <>
       <nav className={`${styles.sidebar} ${isOpen ? styles.sidebarOpen : styles.sidebarClose}`} ref={sidebarRef} aria-label="sidebar" aria-hidden={!isOpen}>
         <div className="sidebar-header">
   
@@ -156,7 +156,7 @@ const Sidebar = () => {
         )}
       </nav>
       <div className={`${styles.sidebarOverlay} ${isOpen ? styles.sidebarOpenOverlay : styles.sidebarCloseOverlay}`} onClick={closeSidebar} aria-label="sidebar overlay" aria-hidden={!isOpen}></div>
-    }
+    </>}
     </>
   )
 }
