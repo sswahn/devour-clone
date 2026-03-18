78677bc53174e25d1e5a9e90460a037f7e079036
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-18 13:46:16 -0400

    Update and rename XmarkIcon.js to XmarkIcon.js

--- src/components/Icons/XmarkIcon/XmarkIcon.js
+++ src/components/Icons/XmarkIcon/XmarkIcon.js
@@ -1,5 +1,5 @@
 
-function xMarkIcon() {
+function XmarkIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
       {/* Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc. */}
