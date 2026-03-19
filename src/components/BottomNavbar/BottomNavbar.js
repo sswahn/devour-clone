e9f90d539ede0fcd16f36d709d57991bccdd6304
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-19 17:47:29 -0400

    Update BottomNavbar.js

--- src/components/BottomNavbar/BottomNavbar.js
+++ src/components/BottomNavbar/BottomNavbar.js
@@ -70,12 +70,13 @@ function BottomNavbar() {
       velocityRef.current = 0
       return setVisible(nav)
     }
-  
-    const isSlowing = Math.abs(velocity) < INTENT_VELOCITY
-    const isReversing = velocity < 0 && velocityRef.current > 0
 
+    const prevVelocity = velocityRef.current
     velocityRef.current = velocity
     scrollYRef.current = scrollY
+  
+    const isSlowing = Math.abs(velocity) < INTENT_VELOCITY
+    const isReversing = velocity < 0 && prevVelocity > 0
 
     if (isSlowing || isReversing) {
       triggerIntent()
