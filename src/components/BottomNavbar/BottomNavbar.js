429c64914c328a810e622ec250918a39afdb2665
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-19 09:45:05 -0400

    Update BottomNavbar.js

--- src/components/BottomNavbar/BottomNavbar.js
+++ src/components/BottomNavbar/BottomNavbar.js
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import styles from './bottomnavbar.module.css'
 
 function BottomNavbar() {
+  const ref = useRef()
 
+  const onScroll = () => {
+    let lastScrollY = window.scrollY
+    const nav = document.querySelector('.bottom-nav')
+    
+    window.addEventListener('scroll', () => {
+      const currentScrollY = window.scrollY
+    
+      if (currentScrollY > lastScrollY && currentScrollY > 50) {
+        ref.current.classList.add('bottom-nav--hidden') // scrolling down
+      } else {
+        ref.current.classList.remove('bottom-nav--hidden') // scrolling up
+      }
+      lastScrollY = currentScrollY
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll)
+    return () => {
+      window.removeEventListener('scroll', onScroll)
+    }
+  }, [])
+  
   // break each button and their function out into components
   
   return (
-    <nav className={styles.bottomNavbar} aria-label="primary navigation">
+    <nav ref={ref} className={styles.bottomNavbar} aria-label="primary navigation">
       <button type="button" aria-label="home">
       </button>
       <button type="button" aria-label="search">
