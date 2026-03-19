7b81341229926debc8ed0cd2ada39e7ce44edacc
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-19 09:17:25 -0400

    Create BottomNavbar.js

---
+++ src/components/BottomNavbar/BottomNavbar.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import styles from './bottomnavbar.module.css'
+
+function BottomNavbar() {
+
+  // break each button and their function out into components
+  
+  return (
+    <nav className={styles.bottomNavbar} aria-label="primary navigation">
+      <button type="button" aria-label="home">
+      </button>
+      <button type="button" aria-label="search">
+      </button>
+      <button type="button" aria-label="camera">
+      </button>
+      <button type="button" aria-label="notifications">
+      </button>
+      <button type="button" aria-label="profile">
+      </button>
+    </nav>
+  )
+}
+
+export default BottomNavbar
