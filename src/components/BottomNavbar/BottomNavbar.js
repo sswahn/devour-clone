18edeb0b0ffc18b93a7847faf5fee13d97776497
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-21 17:09:46 -0400

    Update BottomNavbar.js

--- src/components/BottomNavbar/BottomNavbar.js
+++ src/components/BottomNavbar/BottomNavbar.js
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react'
 import useScrollEffect from '../../hooks/useScrollEffect'
-import scroll from '../../utilities/scrollEngine'
 import HomeIcon from '../Icons/HomeIcon/HomeIcon'
 import SearchIcon from '../Icons/SearchIcon/SearchIcon'
 import CameraIcon from '../Icons/CameraIcon/CameraIcon'
@@ -12,64 +11,6 @@ function BottomNavbar() {
   const navRef = useRef(null)
   const { scrollEffect } = useScrollEffect()
 
-  /*
-  const isHidden = useRef(false)
-  const highVelocity = useRef(false)
-
-  const setHidden = nav => {
-    if (!isHidden.current) {
-      nav.classList.add(styles.hidden)
-      isHidden.current = true
-    }
-  }
-
-  const setVisible = nav => {
-    if (isHidden.current) {
-      nav.classList.remove(styles.hidden)
-      isHidden.current = false
-    }
-  }
-
-  // create a hook called useScrollEffect()
-  // returns a function: scrollEffect
-  // execute in useEffect: navRef && scrollEffect(navRef)
-  
-  const updateNav = ({ deltaY, direction, velocity }) => {
-    const nav = navRef.current
-    if (!nav) {
-      return
-    }
-
-    if (!highVelocity.current && velocity > 70) {
-      highVelocity.current = true
-      return setVisible(nav)
-    }
-
-    if (highVelocity.current && velocity === 0) {
-      highVelocity.current = false
-      return
-    }
-
-    if (highVelocity.current) {
-      return
-    }
-
-    if (direction === 'down' && deltaY > 200) {
-      return setHidden(nav)
-    }
-
-    if (direction === 'up') {
-      return setVisible(nav)
-    }
-  }
-
-  useEffect(() => {
-    const unsubscribe = scroll.subscribe(updateNav)
-    return () => {
-      unsubscribe()
-    }
-  }, [])
-  */
   useEffect(() => {
     navRef && scrollEffect(navRef.current, styles.hidden)
   }, [])
