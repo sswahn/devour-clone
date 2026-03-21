95997fa34104d42fd783fe7113fa3f11d022e653
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-21 11:37:32 -0400

    Update scrollEngine.js

--- src/utilities/scrollEngine.js
+++ src/utilities/scrollEngine.js
@@ -4,7 +4,6 @@ let subscribers = new Set()
 let started = false
 let deltaY = 0
 let scrollStart = 0
-let scrollEnd = 0
 let scrollDirection = undefined
 let ticking = false
 let prevScrollY = 0
@@ -19,10 +18,13 @@ function notify(data) {
 
 function update() {
   const scrollY = window.scrollY
+
+  // Calculate change in Y
+  deltaY = scrollY - scrollStart 
   
   // Calculate current scroll direction
   const dY = scrollY - prevScrollY
-  scrollDirection = dY > 0 ? 'down' : dY < 0 ? 'up' : undefined
+  direction = dY > 0 ? 'down' : dY < 0 ? 'up' : undefined
 
   // Set prevScrollY for use in next frame
   prevScrollY = scrollY
@@ -36,9 +38,7 @@ function update() {
   
   notify({
     deltaY,
-    scrollStart,
-    scrollEnd,
-    scrollDirection,
+    direction,
     scrollY,
     velocity
   })
@@ -58,19 +58,15 @@ function onScroll(event) {
 function onScrollEnd(event) {
   const scrollY = window.scrollY
   
-  // Scroll end point
-  scrollEnd = scrollY
+  // Scroll end point (no use case atm)
+//  scrollEnd = scrollY
   
-  // Calculate change in Y
-  deltaY = scrollEnd - scrollStart 
-  
+
   // Set new scroll start point
-  scrollStart = scrollEnd
+  scrollStart = scrollY
   
   notify({
     deltaY,
-    scrollStart,
-    scrollEnd,
     scrollDirection,
     scrollY,
     velocity
@@ -98,8 +94,6 @@ const scroll = {
     
     fn({
       deltaY,
-      scrollStart,
-      scrollEnd,
       scrollDirection,
       scrollY: window.scrollY,
       velocity
