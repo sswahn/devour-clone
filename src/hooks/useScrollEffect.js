bd0dd49ce311d20e0dda1089736f29226e9e4a4c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-21 15:31:21 -0400

    Update useScrollEffect.js

--- src/hooks/useScrollEffect.js
+++ src/hooks/useScrollEffect.js
@@ -1,7 +1,15 @@
 import { useRef, useEffect } from 'react'
 
 function useScrollEffect() {
-  
+  const element = useRef(null)
+
+  const setElement = el => {
+    element.current = el
+  }
+
+  return {
+    setElement
+  }
 }
 
 export default useScrollEffect
