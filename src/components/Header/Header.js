255a3a885c8a586117472c20eb05a5634205d2ba
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-21 17:10:03 -0400

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import { useContext, useEffect, useRef  } from 'react'
 import { Context } from '../../Provider'
 import useScrollEffect from '../../hooks/useScrollEffect'
-import scroll from '../../utilities/scrollEngine'
 import HomeIcon from '../Icons/HomeIcon/HomeIcon'
 import SearchIcon from '../Icons/SearchIcon/SearchIcon'
 import styles from './header.module.css'
