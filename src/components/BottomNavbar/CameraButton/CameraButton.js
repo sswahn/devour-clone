7cbe414467bcfed849348261c634363c7383dad6
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-21 20:05:30 -0400

    Update CameraButton.js

--- src/components/BottomNavbar/CameraButton/CameraButton.js
+++ src/components/BottomNavbar/CameraButton/CameraButton.js
@@ -1,8 +1,16 @@
 
+import styles from './camerabutton.module.css'
+
 function CameraButton() {
 
+  const onClick = event => {
+    // open camera
+  }
+  
   return (
-    
+    <button className={styles.cameraButton} type="button" aria-label="camera">
+      <CameraIcon />  
+    </button>
   )
 }
 
