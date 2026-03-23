a23f15e81074e7c260367118ba535d7f5d721e3a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-23 15:14:08 -0400

    Create ProfileButton.js

---
+++ src/components/BottomNavbar/ProfileButton/ProfileButton.js
@@ -0,0 +1,11 @@
+import UserIcon from '../../Icons/UserIcon/UserIcon'
+import styles from './profilebutton.module.css'
+
+function ProfileButton() {
+
+  return (
+    
+  )
+}
+
+export default ProfileButton
