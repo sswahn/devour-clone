d07b64a923cd0397512661e3052f8fb7bdcf7732
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-23 15:08:46 -0400

    Update NotificationsButton.js

--- src/components/BottomNavbar/NotificationsButton/NotificationsButton.js
+++ src/components/BottomNavbar/NotificationsButton/NotificationsButton.js
@@ -1,8 +1,17 @@
+import BellIcon from '../../Icons/BellIcon/BellIcon'
+import styles from './notificationsButton.module.css'
+
 
 function NotificationsButton() {
 
+  const onClick = event => {
+    return
+  }
+  
   return (
-    
+    <button className={styles.notificationsButton} onClick={onClick} type="button" aria-label="notifications">
+      <BellIcon />  
+    </button>
   )
 }
 
