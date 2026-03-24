d26204130170c4fed9a53ae24e8d72fddfe6310e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-24 13:10:37 -0400

    Update Avatar.js

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -3,6 +3,7 @@ import UserIcon from '../Icons/UserIcon/UserIcon'
 import styles from './styles.module.css'
 
 const Avatar = ({ className, image, username, onClick, size }) => {
+  // should onClick be a prop or defined here?
   return (
     <div className={`${styles.avatar} ${className || ''}`} onClick={onClick} aria-label={`${username}'s avatar`}>
       {image 
