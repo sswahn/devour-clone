ca01e23131b1738c594ee733b5e57bf78a80457f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-24 10:06:30 -0400

    Update HomeButton.js

--- src/components/BottomNavbar/HomeButton/HomeButton.js
+++ src/components/BottomNavbar/HomeButton/HomeButton.js
@@ -6,7 +6,7 @@ function HomeButton() {
   const onClick = event => {
     // make hook?
     if (window.navigator && window.navigator.vibrate) {
-      navigator.vibrate(10)
+      navigator.vibrate(50)
     }
     
     return
