e67ef54a36dabbdb6d46ae53330dc2c63734e500
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-24 10:33:26 -0400

    Update HomeButton.js

--- src/components/BottomNavbar/HomeButton/HomeButton.js
+++ src/components/BottomNavbar/HomeButton/HomeButton.js
@@ -7,7 +7,7 @@ function HomeButton() {
     // Try 50 if this is too high (last working value was 100)
     // "10-20ms may be too subtle for most users."
     if (window.navigator && window.navigator.vibrate) {
-      navigator.vibrate(75) 
+      navigator.vibrate(50) 
     }
     
     return
