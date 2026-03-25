0f009852398f9953fe7cc452d40ee5d6987fba99
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-25 15:57:42 -0400

    Update SearchForm.js

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -12,19 +12,18 @@ function SearchForm() {
   
   const onChange = event => {
     // debounce
-    //
     // store recent searches in locoalStorage
     // avoid multiple http requests for the same query
     return
   }
   
-  const onKeyPress = event => {
+  const onKeyDown = event => {
     // handle for enter key (prolly ignore it)
     // handle tab to suggestions
     // escape clears + closes if already cleared
     // escape -> clearCloseInput()
-
     // arrow keys up and down suggestions
+    
     return
   }
 
@@ -32,20 +31,21 @@ function SearchForm() {
     event.preventDefault()
   }
   
-
   return (
-    <form className={styles.searchForm}>
+    <form className={styles.searchForm} onSubmit={onSubmit}>
       <input 
         ref={inputRef}
         type="search"
-        value={currentValue}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-        placeholder={placeholder}
-        disabled={disabled}
+        value={searchValue}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        placeholder="Search"
+        disabled={false}
         aria-label="search input"  
       />
       <button type="button" onClick={clearCloseInput} aria-label="clear search">x</button>
+          
+      <div>Suggestions</div>
     </form>
   )
 }
