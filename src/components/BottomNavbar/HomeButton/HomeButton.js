d076b8bc056cc1744acad50a53df368615ce73d1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-25 19:53:07 -0400

    Update HomeButton.js

--- src/components/BottomNavbar/HomeButton/HomeButton.js
+++ src/components/BottomNavbar/HomeButton/HomeButton.js
@@ -4,11 +4,12 @@ import styles from './homebutton.module.css'
 function HomeButton() {
 
   const onClick = event => {
-    if (window.navigator && window.navigator.vibrate) {
-      navigator.vibrate(50)
+    if (window.scrollY !== 0) {
+      window.scrollTo({
+        behavior: 'smooth',
+        top: 0
+      })
     }
-    
-    return
   }
   
   return (
