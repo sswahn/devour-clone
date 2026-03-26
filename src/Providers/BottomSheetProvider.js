6bbf458c3cf683b029f554ea3316bb1c17b8428f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-26 11:22:15 -0400

    Create BottomSheetProvider.js

---
+++ src/Providers/BottomSheetProvider.js
@@ -0,0 +1,30 @@
+import { createContext, useReducer } from 'react'
+
+const BottomSheetContext = createContext(null)
+const BottomSheetDispatch = createContext(null)
+
+const initialState = {
+  bottomsheet: false,
+  modal: false
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'bottomsheet':
+      return { ...state, bottomsheet: action.payload }
+    default:
+      return state
+  }
+}
+
+export function UIProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <UIContext.Provider value={state}>
+      <UIDispatch.Provider value={dispatch}>
+        {children}
+      </UIDispatch.Provider>
+    </UIContext.Provider>
+  )
+}
