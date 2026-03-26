0565c525c0f9a10fb9764547ec780c0442c3c60a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-26 10:22:09 -0400

    Update NotificationsButton.js

--- src/components/BottomNavbar/NotificationsButton/NotificationsButton.js
+++ src/components/BottomNavbar/NotificationsButton/NotificationsButton.js
@@ -1,12 +1,16 @@
+import { useContext } from 'state'
+import { Context } from '../../../Provider'
 import BellIcon from '../../Icons/BellIcon/BellIcon'
 import styles from './notificationsbutton.module.css'
 
 function NotificationsButton() {
+  const [context, dispatch] = useContext(Context)
 
   const onClick = event => {
     if (window.navigator && window.navigator.vibrate) {
       navigator.vibrate(50)
     }
+    // dispatch({ type: 'drawer', payload: true })
     return
   }
   
