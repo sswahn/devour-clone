0c3674093085ccce52332f5f54b392b569f4b988
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-26 13:30:19 -0400

    Update SearchFormProvider.js

--- src/Providers/SearchFormProvider.js
+++ src/Providers/SearchFormProvider.js
@@ -1,8 +1,31 @@
+import { createContext, useReducer } from 'react'
 
+const SearchFormContext = createContext(null)
+const SearchFormDispatch = createContext(null)
 
-function SearchFormProvider() {
-
-  return ()
+const initialState = {
+  searchform: false
 }
 
-export default 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'searchform':
+      return { ...state, searchform: action.payload }
+    default:
+      return state
+  }
+}
+
+export function SearchFormProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <SearchFormContext.Provider value={state}>
+      <SearchFormDispatch.Provider value={dispatch}>
+        {children}
+      </SearchFormDispatch.Provider>
+    </SearchFormContext.Provider>
+  )
+}
+
+export default SearchFormProvider
