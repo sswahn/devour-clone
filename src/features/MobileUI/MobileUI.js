9988139a2f2c88292f7e7fcc1b5a48c843daacd0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-26 13:40:25 -0400

    Update MobileUI.js

--- src/features/MobileUI/MobileUI.js
+++ src/features/MobileUI/MobileUI.js
@@ -1,31 +1,26 @@
+import { useContext } from 'react'
+import { BottomSheetContext } from './Providers/BottomSheetContext'
+
 import styles from './mobileui.module.css'
 
 function MobileUI() { 
+  const [{bottomsheet}] = useContext(BottomSheetContext)
+  const [context] = useContext(SearchFormContext)
+  const [context] = useContext(CameraContext)
+  
   const bottomSheetOpen = useBottomSheet()
   const searchOpen = useSearch()
   const cameraActive = useCamera()
 
   return createPortal(
       <section className={styles.mobileUi}>
-        {bottomSheetOpen && <BottomSheet />}
+        {bottomsheet && <BottomSheet />}
         {searchOpen && <SearchForm />}
         {cameraActive && <Camera />}
       </section>, 
       document.body
     )
   )
-       
-    {/* the following should go in App 
-    <BottomSheetProvider>
-      <SearchProvider>
-        <CameraProvider>
-          <Main />       
-          <UIRoot />     
-          <BottomNav />  {/* independent, (dispatch only) stable 
-        </CameraProvider>
-      </SearchProvider>
-    </BottomSheetProvider>
-  ) */}
 }
 
 export default MobileUI
