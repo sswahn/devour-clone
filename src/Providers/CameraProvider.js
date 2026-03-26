e5540790f6aa2ab3f4fb35845be8326378a26471
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-26 13:32:29 -0400

    Create CameraProvider.js

---
+++ src/Providers/CameraProvider.js
@@ -0,0 +1,31 @@
+import { createContext, useReducer } from 'react'
+
+const CameraContext = createContext(null)
+const CameraDispatch = createContext(null)
+
+const initialState = {
+  camera: false
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'camera':
+      return { ...state, camera: action.payload }
+    default:
+      return state
+  }
+}
+
+function CameraProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <CameraContext.Provider value={state}>
+      <CameraDispatch.Provider value={dispatch}>
+        {children}
+      </CameraDispatch.Provider>
+    </CameraContext.Provider>
+  )
+}
+
+export default CameraProvider
