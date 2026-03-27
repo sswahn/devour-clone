80c60e79164b1e5c26d506240f25c8d25d6bf2e3
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-27 11:23:30 -0400

    Update Feed.js

--- src/features/Feed/Feed.js
+++ src/features/Feed/Feed.js
@@ -36,9 +36,9 @@ function Feed() {
   return (
     <section className={styles.feed} role="feed" aria-busy={loading}>
 
-      {data.map((item, index) => {
-        <FeedNode key={item.id} item={item} index={index + 1} count={data.length} />
-      })}
+      {data.map((item, index) => 
+        <FeedNode key={index} item={item} index={index + 1} count={data.length} />
+      )}
 {/*
       {data.map((item, index) => {
         <FeedNode key={item.id} item={item} index={idex + 1} count={items.length} />
