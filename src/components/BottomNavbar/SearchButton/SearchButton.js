7f94e1a4712314924e6456364a8f83481ade9b6a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-27 09:54:00 -0400

    Update SearchButton.js

--- src/components/BottomNavbar/SearchButton/SearchButton.js
+++ src/components/BottomNavbar/SearchButton/SearchButton.js
@@ -1,14 +1,16 @@
+import { useContext } from 'react'
+import { SetSearchFormContext } from '../Providers/SearchFormProvider'
 import SearchIcon from '../../Icons/SearchIcon/SearchIcon'
 import styles from './searchbutton.module.css'
 
 function SearchButton() {
-
+  const setSearchFormContext = useContext(SetSearchFormContext)
+  
   const onClick = event => {
     if (window.navigator && window.navigator.vibrate) {
       navigator.vibrate(50)
     }
-    
-    return
+    setSearchFormContext(prevContext => !prevContext)
   }
   
   return (
