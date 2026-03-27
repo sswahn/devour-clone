9341a050cd6df885819790079d3d2e2e28ee36de
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-27 09:48:00 -0400

    Update MobileUI.js

--- src/components/MobileUI/MobileUI.js
+++ src/components/MobileUI/MobileUI.js
@@ -1,23 +1,23 @@
 import { useContext } from 'react'
 import { createPortal } from 'react-dom'
-import { BottomSheetContext } from '../Providers/BottomSheetProvider'
-import { SearchFormContext } from '../Providers/SearchFormProvider'
-import { CameraContext } from '../Providers/CameraProvider'
+import { GetBottomSheetContext } from '../Providers/BottomSheetProvider'
+import { GetSearchFormContext } from '../Providers/SearchFormProvider'
+import { GetCameraContext } from '../Providers/CameraProvider'
 import BottomSheet from '../BottomSheet/BottomSheet'
 import SearchForm from '../SearchForm/SearchForm'
 import Camera from '../../features/Camera/Camera'
 import styles from './mobileui.module.css'
 
 function MobileUI() { 
-  const bottomsheet = useContext(BottomSheetContext)
-  const searchform = useContext(SearchFormContext)
-  const camera = useContext(CameraContext)
+  const getBottomSheet = useContext(GetBottomSheetContext)
+  const getSearchForm = useContext(GetSearchFormContext)
+  const getCameraContext = useContext(GetCameraContext)
 
   return createPortal(
     <section className={styles.mobileUi}>
-      {bottomsheet && <BottomSheet />}
-      {searchform && <SearchForm />}
-      {camera && <Camera />}
+      {getBottomSheet && <BottomSheet />}
+      {getSearchForm && <SearchForm />}
+      {getCameraContext && <Camera />}
     </section>, 
     document.body
   )
