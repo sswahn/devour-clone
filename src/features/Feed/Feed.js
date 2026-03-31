a57072021b663f92af6ca52066c728fb736b64fb
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-31 17:25:09 -0400

    Update Feed.js

--- src/features/Feed/Feed.js
+++ src/features/Feed/Feed.js
@@ -8,7 +8,7 @@ import Sentinel from './Sentinel' // sentinel triggers infinite loading
 import styles from './feed.module.css'
 
 function Feed() {
-  const scrollRef = useScrollContext(ref => scroll.setElement(ref))
+  const scrollRef = useScrollContext(ref => ref.current && scroll.setElement(ref.current))
   const [data, setData] = useState([1,2,3])
   const [batchNumber, setBatchNumber] = useState(0)
   const [loading, setLoading] = useState(false)
