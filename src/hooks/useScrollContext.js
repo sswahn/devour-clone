f9192dfe3ddce7f4e9bee8295301d0be520b5587
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-31 09:51:42 -0400

    Update useScrollContext.js

--- src/hooks/useScrollContext.js
+++ src/hooks/useScrollContext.js
@@ -3,7 +3,9 @@ import { ScrollContext } from '../components/Providers/ScrollProvider'
   
 function useScollContext() {
   const ref = useContext(ScrollContext)
-
+  if (!ref) {
+    throw new Error('useScrollContext must be used within appropriate context provider.')
+  }
   return ref
 }
 
