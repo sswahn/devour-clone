c444227cdbae1f8372dd4945e26f62c5b477c19e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-31 09:29:34 -0400

    Update CameraProvider.js

--- src/components/Providers/CameraProvider.js
+++ src/components/Providers/CameraProvider.js
@@ -6,9 +6,15 @@ const SetCameraContext = createContext(null)
 function CameraProvider({ children }) {
   const [state, setState] = useState(false)
 
+  const context = useMemo(() => state, [state])
+
+  const setContext = useCallback(() => {
+    setState(prevState => !prevState)
+  }, [])
+
   return (
-    <GetCameraContext.Provider value={state}>
-      <SetCameraContext.Provider value={setState}>
+    <GetCameraContext.Provider value={context}>
+      <SetCameraContext.Provider value={setContext}>
         {children}
       </SetCameraContext.Provider>
     </GetCameraContext.Provider>
