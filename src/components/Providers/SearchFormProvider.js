0fc14fa3e3bf762a3d0a26879cde7dd984640b7e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-31 07:44:14 -0400

    Update SearchFormProvider.js

--- src/components/Providers/SearchFormProvider.js
+++ src/components/Providers/SearchFormProvider.js
@@ -1,14 +1,25 @@
-import { useState, createContext } from 'react'
+import { useState, useMemo, useCallback, createContext } from 'react'
 
 const GetSearchFormContext = createContext(null)
 const SetSearchFormContext = createContext(null)
 
+// providers should have useHook implementation
+// between context and components
+// probably one hook per context:
+// useGetSearchFormContext, etc.
+
 function SearchFormProvider({ children }) {
   const [state, setState] = useState(false)
 
+  const context = useMemo(() => state, [state])
+
+  const setContext = useCallback(() => {
+    setState(prevState => !prevState)
+  }, [])
+
   return (
-    <GetSearchFormContext.Provider value={state}>
-      <SetSearchFormContext.Provider value={setState}>
+    <GetSearchFormContext.Provider value={context}>
+      <SetSearchFormContext.Provider value={setContext}>
         {children}
       </SetSearchFormContext.Provider>
     </GetSearchFormContext.Provider>
