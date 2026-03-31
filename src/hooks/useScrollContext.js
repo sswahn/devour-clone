12ad0bb7e674cb8e2f8a65f99e40a70c5194d7df
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-31 17:33:32 -0400

    Update useScrollContext.js

--- src/hooks/useScrollContext.js
+++ src/hooks/useScrollContext.js
@@ -1,6 +1,9 @@
 import { useContext } from 'react'
 import { ScrollContext } from '../components/Providers/ScrollProvider'
-  
+
+// check with AI about using provider/context/hook pattern
+// vs utility with getter setter for this functionality
+
 function useScrollContext(callback = undefined) {
   const ref = useContext(ScrollContext)
   if (!ref) {
