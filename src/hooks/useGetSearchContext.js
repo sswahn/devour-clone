525296e83f5f7723a6cf045db8a768e7ecff0e26
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-31 09:41:41 -0400

    Create useGetSearchContext.js

---
+++ src/hooks/useGetSearchContext.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react'
+import { GetSearchContext } from '../components/Providers/SearchProvider'
+  
+function useGetSearchContext() {
+  const context = useContext(GetSearchContext)
+  if (!context) {
+    throw new Error('useGetSearchContext must be used within appropriate context provider.')
+  }
+  return context
+}
+
+export default useGetSearchContext
