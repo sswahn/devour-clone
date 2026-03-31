163b9191c6923bb8f2f788e634b2d35a62b04fd1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-31 08:01:42 -0400

    Update Providers.js

--- src/Providers.js
+++ src/Providers.js
@@ -1,15 +1,18 @@
 import { ProfileProvider } from './components/Providers/ProfileProvider'
+import { ScrollProvider } from './components/Providers/ScrollProvider'
 import { SearchFormProvider } from './components/Providers/SearchFormProvider'
 import { CameraProvider } from './components/Providers/CameraProvider'
 
 function Providers({ children }) {
   return (
     <ProfileProvider>
-      <SearchFormProvider>
-        <CameraProvider>
-          {children}
-        </CameraProvider>
-      </SearchFormProvider>
+      <ScrollProvider>
+        <SearchFormProvider>
+          <CameraProvider>
+            {children}
+          </CameraProvider>
+        </SearchFormProvider>
+      </ScrollProvider>
     </ProfileProvider>
   )
 }
