0551bfb94d4cf799fcbf3d5343c9c4bbbe7d3109
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-31 09:30:46 -0400

    Update SearchFormProvider.js

--- src/components/Providers/SearchFormProvider.js
+++ src/components/Providers/SearchFormProvider.js
@@ -12,7 +12,6 @@ function SearchFormProvider({ children }) {
   const [state, setState] = useState(false)
 
   const context = useMemo(() => state, [state])
-
   const setContext = useCallback(() => {
     setState(prevState => !prevState)
   }, [])
