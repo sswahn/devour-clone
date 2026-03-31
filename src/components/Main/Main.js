0ce5e83228d1fc4996be3bdf3e5d6421aca2ac95
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-31 15:18:07 -0400

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,8 +2,8 @@ import { useState, useContext, useEffect, Suspense, lazy } from 'react'
 import { Context } from '../../Provider'
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
 import Feed from '../../features/Feed/Feed'
-// const MobileUI = lazy(() => import('../MobileUI/MobileUI'))
-import MobileUI from '../MobileUI/MobileUI'
+const MobileUI = lazy(() => import('../MobileUI/MobileUI'))
+//import MobileUI from '../MobileUI/MobileUI'
 import styles from './main.module.css'
 
 function Main() {
