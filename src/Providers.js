f79a412febf12c5ff7893a4c1f96e91765455573
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-31 09:58:28 -0400

    Update Providers.js

--- src/Providers.js
+++ src/Providers.js
@@ -11,7 +11,7 @@ function Providers({ children }) {
           <CameraProvider>
             {children}
           </CameraProvider>
-        </SearchFormProvider>
+        </SearchProvider>
       </ScrollProvider>
     </ProfileProvider>
   )
