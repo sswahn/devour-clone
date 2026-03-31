7c2637848e1eb3d171388959120baa571b015b6c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-03-31 10:22:40 -0400

    Update MobileUI.js

--- src/components/MobileUI/MobileUI.js
+++ src/components/MobileUI/MobileUI.js
@@ -4,7 +4,8 @@ import { createPortal } from 'react-dom'
 // change to custom hook access of context
 import { GetCameraContext } from '../Providers/CameraProvider'
 import { GetProfileContext } from '../Providers/ProfileProvider'
-import { GetSearchContext } from '../Providers/SearchProvider'
+
+import useGetSearchContext from '../../hooks/useGetSearchContext'
 
 import Camera from '../../features/Camera/Camera'
 import BottomSheet from '../BottomSheet/BottomSheet' // Change to Profile Component
@@ -14,13 +15,13 @@ import styles from './mobileui.module.css'
 function MobileUI() { 
   const getCameraContext = useContext(GetCameraContext)
   const getProfileContext = useContext(GetProfileContext)
-  const getSearchFormContext = useContext(GetSearchFormContext)
+  const searchContext = useGetSearchContext()
   
   return createPortal(
     <section className={styles.mobileUi}>
       {getCameraContext && <Camera />}
       {getProfileContext && <BottomSheet />} {/* Change to <Profile />*/}
-      {getSearchFormContext && <SearchForm />}
+      {searchContext && <SearchForm />}
     </section>, 
     document.body
   )
