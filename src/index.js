a62917f5f77bc8348f029842818f7ddb59669efc
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-01 17:58:11 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 //import * as serviceWorker from './serviceWorker'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import logError from "./utilities/logError"
+
+// Global error listeners
+window.addEventListener('error', event => logError(event.error))
+window.addEventListener('unhandledrejection', event => logError(event.reason))
 
 createRoot(document.getElementById('root')).render(<App />)
 
