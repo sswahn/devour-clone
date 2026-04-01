00d49d566fa3c7d1ef66e34d404e7223e1ec3f22
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-01 08:34:51 -0400

    Update useScrollContext.js

--- src/hooks/useScrollContext.js
+++ src/hooks/useScrollContext.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import { ScrollContext } from '../components/Providers/ScrollProvider'
 
 function useScrollContext(callback = undefined) {
@@ -6,19 +6,6 @@ function useScrollContext(callback = undefined) {
   if (!ref) {
     throw new Error(`Error in useScrollContext. ref: ${ref}`)
   }
-
-  /*
-  useEffect(() => {
-    if (!ref.current) {
-      return
-    }
-
-    if (callback) {
-      callback(ref)
-    }
-    
-  }, [ref, callback])
-*/
   return ref
 }
 
