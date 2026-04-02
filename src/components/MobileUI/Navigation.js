fcaf30cffff4194a46813f8ccccad5a279b3e488
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-02 09:01:18 -0400

    Update Navigation.js

--- src/components/MobileUI/Navigation.js
+++ src/components/MobileUI/Navigation.js
@@ -1,13 +1,38 @@
-import BottomNavbar from '../BottomNavbar/BottomNavbar'
+import { useEffect, useRef } from 'react'
+import useScrollEffect from '../../hooks/useScrollEffect'
+import HomeButton from './HomeButton/HomeButton'
+import SearchButton from './SearchButton/SearchButton'
+import CameraButton from './CameraButton/CameraButton'
+import NotificationsButton from './NotificationsButton/NotificationsButton'
+import ProfileButton from './ProfileButton/ProfileButton'
+import styles from './navigation.module.css'
 
 function Navigation({ openSearch, openCamera, openNotifications, openProfile }) {
+  const navRef = useRef(null)
+  const { scrollEffect } = useScrollEffect()
+
+  useEffect(() => {
+    navRef && scrollEffect(navRef.current, styles.hidden)
+  }, [])
+  
   return (
-    <BottomNavbar 
-      openSearch={openSearch} 
-      openCamera={openCamera} 
-    
-    />
+    <nav ref={navRef} className={styles.navigation} aria-label="primary navigation">
+      <div>
+        <div>
+          <HomeButton />  
+          <SearchButton openSearch={openSearch} />
+        </div>
+        <div>
+          <CameraButton openCamera={openCamera} />
+        </div>
+        <div>
+          <NotificationsButton openNotifications={openNotifications} />
+          <ProfileButton openProfile={openProfile} />
+        </div>
+      </div>
+    </nav>
   )
 }
 
 export default Navigation
+export default BottomNavbar
