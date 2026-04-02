f0822356eee9b9803f12cadb8f49b4bb4a1afb64
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-02 08:37:23 -0400

    Update SearchButton.js

--- src/components/BottomNavbar/SearchButton/SearchButton.js
+++ src/components/BottomNavbar/SearchButton/SearchButton.js
@@ -1,16 +1,14 @@
-import { useSetSearchContext } from '../../../hooks/useSearchContext'
 import SearchIcon from '../../Icons/SearchIcon/SearchIcon'
 import styles from './searchbutton.module.css'
 
-function SearchButton() {
-  const setSearchContext = useSetSearchContext()
-  
+function SearchButton({ openSearch }) {
+
   const onClick = async event => {
     try {
       navigator.vibrate(50)
       await document.getElementById('portal').requestFullscreen()
       await screen.orientation.lock('portrait')
-      setSearchContext(prevContext => !prevContext)
+      openSearch()
     } catch (error) {
       throw new Error(error)
     }
