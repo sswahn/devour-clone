ac5668acb7a04c2323da97a5ec4b7fb571b17c77
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-02 09:10:29 -0400

    Create CameraButton.js

---
+++ src/components/MobileUI/Navigation/CameraButton/CameraButton.js
@@ -0,0 +1,26 @@
+import CameraIcon from '../../../Icons/CameraIcon/CameraIcon'
+import styles from './camerabutton.module.css'
+
+function CameraButton({ openCamera }) {
+  const setCameraContext = useContext(SetCameraContext)
+  
+  const onClick = async event => {
+    try {
+      navigator.vibrate(50)
+      return
+      await document.getElementById('portal').requestFullscreen()
+      await screen.orientation.lock('portrait')
+      openCamera()
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+  
+  return (
+    <button className={styles.cameraButton} onClick={onClick} type="button" aria-label="open camera">
+      <CameraIcon size={32} />  
+    </button>
+  )
+}
+
+export default CameraButton
