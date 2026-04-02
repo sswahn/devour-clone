bd42d4c1982364b7bed681e59c7dad761bc0911f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-02 12:26:25 -0400

    Update SearchForm.js

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -11,7 +11,7 @@ function SearchForm({ closeSearch }) {
 
   const handleCloseSearch = async event => {
     try {
-     await document.exitFullscreen()
+    // await document.exitFullscreen()
      closeSearch()
     } catch (error) {
       throw new Error(error)
