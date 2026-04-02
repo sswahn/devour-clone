ec60c6cefbe69365889d67a2e5bdf5b48a17cbf3
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-02 09:42:22 -0400

    Update Interface.js

--- src/components/MobileUI/Interface.js
+++ src/components/MobileUI/Interface.js
@@ -1,17 +1,26 @@
-import { useContext } from 'react'
+import { Suspense, lazy } from 'react'
 import { createPortal } from 'react-dom'
-import Camera from '../../features/Camera/Camera'
-import BottomSheet from '../BottomSheet/BottomSheet' // Change to Profile Component
-import SearchForm from '../SearchForm/SearchForm'
-import styles from './mobileui.module.css'
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
+const SearchForm = lazy(() => import('../SearchForm/SearchForm'))
+const Camera = lazy(() => import('../../features/Camera/Camera'))
+const BottomSheet = lazy(() => import('../BottomSheet/BottomSheet')) // Change to Profile Component
+const styles from './mobileui.module.css'
 
 function Interface({ search, camera, notifications, profile, closeSearch, closeCamera, closeNotifications, closeProfile }) {
   return createPortal(
     <section className={styles.mobileUi}>
-      {search && <SearchForm closeSearch={closeSearch} />}
-      {camera && <Camera />}
-      {/* notifications && <Notifications closeNotifications={closeNotifications} /> */}
-      {profile && <BottomSheet />} {/* Change to <Profile />*/}
+      <Suspense fallback={<LoadingSpinner />}>
+        {search && <SearchForm closeSearch={closeSearch} />}
+      </Suspense>
+      <Suspense fallback={<LoadingSpinner />}>
+        {camera && <Camera closeCamera={closeCamera} />}
+      </Suspense>
+     {/* <Suspense fallback={<LoadingSpinner />}>
+       notifications && <Notifications closeNotifications={closeNotifications} /> 
+      </Suspense> */}
+      <Suspense fallback={<LoadingSpinner />}>    
+        {profile && <BottomSheet closeProfile={closeProfile} />} {/* Change to <Profile />*/}
+      </Suspense>
     </section>, 
     document.getElementById('portal')
   )
