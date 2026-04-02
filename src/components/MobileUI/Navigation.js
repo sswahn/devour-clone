46b442a37cbc57087a668958afe283dd32192764
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-02 08:58:43 -0400

    Update Navigation.js

--- src/components/MobileUI/Navigation.js
+++ src/components/MobileUI/Navigation.js
@@ -2,7 +2,11 @@ import BottomNavbar from '../BottomNavbar/BottomNavbar'
 
 function Navigation({ openSearch, openCamera, openNotifications, openProfile }) {
   return (
-    <BottomNavbar openSearch={openSearch} />
+    <BottomNavbar 
+      openSearch={openSearch} 
+      openCamera={openCamera} 
+    
+    />
   )
 }
 
