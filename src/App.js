b12867461051e84628f0e8d1d65b34f5ad32d26d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-02 08:55:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import Providers from './Providers'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import MobileUI from './components/MobileUI/MobileUI'
-//import BottomNavbar from './components/BottomNavbar/BottomNavbar'
 import './index.css'
 
 function App() {
   return (
     <React.StrictMode> 
       <ErrorBoundary> 
-        <Providers>
-          <Header />
-          <Main />
-          <MobileUI />
-         {/* <BottomNavbar /> */}
-        </Providers> 
+        <Header />
+        <Main />
+        <MobileUI />
       </ErrorBoundary> 
     </React.StrictMode>
   )
