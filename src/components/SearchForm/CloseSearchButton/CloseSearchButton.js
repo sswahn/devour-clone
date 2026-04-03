c1ed5fbb41ff447af26a8d764cb237c321331ce5
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-03 13:26:45 -0400

    Update CloseSearchButton.js

--- src/components/SearchForm/CloseSearchButton/CloseSearchButton.js
+++ src/components/SearchForm/CloseSearchButton/CloseSearchButton.js
@@ -1,7 +1,13 @@
 import XmarkIcon from '../../Icons/XmarkIcon/XmarkIcon'
 import styles from './CloseSearchButton.module.css'
 
-function CloseSearchButton() {
+function CloseSearchButton({ closeSearch }) {
+
+  const handleCloseSearch = async event => {
+    // await document.exitFullscreen()
+    closeSearch()
+  }
+ 
  return (
    <button className={styles.closeSearchButton} onClick={handleCloseSearch} type="button" aria-label="close search">
      <XmarkIcon />
