a31d75a7c274456eb6277ff53d7969bf245ecbde
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-03 15:33:40 -0400

    Update SpeechRecognitionButton.js

--- src/components/SearchForm/SpeechRecognitionButton/SpeechRecognitionButton.js
+++ src/components/SearchForm/SpeechRecognitionButton/SpeechRecognitionButton.js
@@ -3,19 +3,35 @@ import useSpeechRecognition from '../../../hooks/useSpeechRecognition'
 import MicrophoneIcon from '../../Icons/MicrophoneIcon/MicrophoneIcon'
 import styles from './SpeechRecognitionButton.module.css'
 
-function SpeechRecognitionButton({ setTempTranscript, setFinalTranscript }) {
+/*
+isSupported,
+isListening,
+finalTranscript,
+interimTranscript,
+error,
+start,
+stop,
+reset
+*/
 
-  const speechRecognition = () => {
-    const recognition = useSpeechRecognition()
+function SpeechRecognitionButton({ setTempTranscript, setFinalTranscript }) {
+  const recognition = useSpeechRecognition()
+
+  const handleSpeechRecognition = () => {
     recognition.start()
+
+    console.log('recognition.interimTranscript: ', recognition.interimTranscript)
+    console.log('recognition.finalTranscript: ', recognition.finalTranscript)
     
+    setTempTranscript(recognition.interimTranscript)
+    setFinalTranscript(recognition.finalTranscript)
   }
 
-  return (window.SpeechRecognition || window.webkitSpeechRecognition) && (
-    <button className={styles.speechRecognitionButton} onClick={speechRecognition} type="button" aria-label="voice recognition">
+  return recognition.isSupported && (
+    <button className={styles.speechRecognitionButton} onClick={handleSpeechRecognition} type="button" aria-label="voice recognition">
       <MicrophoneIcon size={18} />
     </button>
   )
 }
 
-export default memo(SpeechRecognitionButton)
+export default SpeechRecognitionButton
