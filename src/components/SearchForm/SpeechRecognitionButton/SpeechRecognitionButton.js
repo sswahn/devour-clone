af494d1413d11debaa1113472303260c85f9cf4f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-03 12:30:52 -0400

    Update SpeechRecognitionButton.js

--- src/components/SearchForm/SpeechRecognitionButton/SpeechRecognitionButton.js
+++ src/components/SearchForm/SpeechRecognitionButton/SpeechRecognitionButton.js
@@ -1,6 +1,15 @@
 
 function SpeechRecognitionButton() {
 
+  // consider using a custom hook
+  const speechRecognition = () => {
+    const recognition = new window.SpeechRecognition() // window.webkitSpeechRecognition
+    recognition.continuous = true
+    recognition.interimResults = true
+    recognition.lang = 'en-US'
+    recognition.start()
+  }
+
   return window.SpeechRecognition || window.webkitSpeechRecognition && 
     <button type="button" onClick={speechRecognition} aria-label="voice recognition">
       <MicrophoneIcon size={18} />
