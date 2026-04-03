8f7fdbb6a4c81dd1c419658daaffcae6cddd923c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-03 15:28:32 -0400

    Update SpeechRecognitionButton.js

--- src/components/SearchForm/SpeechRecognitionButton/SpeechRecognitionButton.js
+++ src/components/SearchForm/SpeechRecognitionButton/SpeechRecognitionButton.js
@@ -4,62 +4,12 @@ import MicrophoneIcon from '../../Icons/MicrophoneIcon/MicrophoneIcon'
 import styles from './SpeechRecognitionButton.module.css'
 
 function SpeechRecognitionButton({ setTempTranscript, setFinalTranscript }) {
-  const recognitionRef = useRef()
-  // consider using a custom hook
+
   const speechRecognition = () => {
-
-    console.log('speechRecognition function fired.')
-    
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-    const recognition = new SpeechRecognition()
-    recognition.continuous = true
-    recognition.interimResults = true
-    recognition.lang = 'en-US'
-
-    recognitionRef.current = recognition
-    
-    console.log('recognition set.')
-
-    
-    recognition.onerror = event => {
-      console.error('recognition error: ', event.error)
-    }
-    recognition.onend = () => {
-      console.log('onend fired.')
-      // to auto restart, call recognition.start()
-    }
-    
-    recognition.onresult = event => { 
-      let temp = ''
-
-      console.log('onresult event fired. looping: ')
-      
-      for (let i = event.resultIndex; i < event.results.length; i++) {
-        const segment = event.results[i][0].transcript
-
-        console.log('event.results[i]: ', event.results[i])
-        console.log('event.results[i][0].transcript: ', segment)
-        
-        if (event.results[i].isFinal) {
-          setFinalTranscript(prev => [...prev, segment])
-          setTempTranscript('')
-        } else {
-          temp += segment
-          
-          console.log('tempTranscript: ', temp)
-          setTempTranscript(temp)
-        }
-      }
-      
-    }
+    const recognition = useSpeechRecognition()
     recognition.start()
+    
   }
-
-  useEffect(() => {
-    return () => {
-      recognitionRef.current.stop()
-    }
-  }, [])
 
   return (window.SpeechRecognition || window.webkitSpeechRecognition) && (
     <button className={styles.speechRecognitionButton} onClick={speechRecognition} type="button" aria-label="voice recognition">
