b965166d6b4249775c1b46e3694cd4f090dffc3f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-04 09:30:24 -0400

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,11 +8,27 @@ function Main() {
     //alert(`width: ${window.innerWidth}`) 
     //alert(`height: ${window.innerHeight}`)
   }, [])
+
+  const chime = event => {
+    const audioCtx = new window.AudioContext()
+    const oscillator = audioCtx.createOscillator()
+    ocillator.type = 'sine'
+    oscillator.frequency.setValueAtTime(440, audioCtx.currentTime)
+    oscillator.connect(audioCtx.destination)
+    oscillator.start()
+    oscillator.stop(audioCtx.currentTime + 1)
+  }
   
   return (
     <main className={styles.main}>
-      {/* <Suggestions /> etc. */}
-      <Feed />
+
+     <button onClick={chime} type="button">
+      Chime 
+     </button>
+
+    
+      {/* <Suggestions /> etc. 
+      <Feed />*/}
     </main>
   )
 }
