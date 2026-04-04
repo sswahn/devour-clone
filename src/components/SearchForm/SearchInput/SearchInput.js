c67961b6f77f9403e126010c0d921a1b90181313
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-04 16:47:15 -0400

    Update SearchInput.js

--- src/components/SearchForm/SearchInput/SearchInput.js
+++ src/components/SearchForm/SearchInput/SearchInput.js
@@ -4,13 +4,10 @@ import styles from './SearchInput.module.css'
 function SearchInput({ searchValue, setSearchValue }) {
 
   const onChange = ({ target }) => {
-    setSearchValue(target.value)    
-  }
-
-  const onInput = ({ target }) => {
     if (target.validity.patternMismatch) {
-      target.reportValidity()
+      return target.reportValidity()
     }
+    setSearchValue(target.value)    
   }
   
   return (
@@ -20,7 +17,6 @@ function SearchInput({ searchValue, setSearchValue }) {
       value={searchValue}
       enterKeyHint="search"
       onChange={onChange}
-      onInput={onInput}
       maxLength="288"
       pattern="[a-zA-Z0-9 ]+" 
       title="Please use letters and numbers only."
