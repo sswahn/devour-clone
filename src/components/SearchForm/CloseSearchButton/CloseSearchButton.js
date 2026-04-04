de9886bf84e7ba6a8cf9b7f9f84b3ed27cd91c22
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-04 12:33:36 -0400

    Update CloseSearchButton.js

--- src/components/SearchForm/CloseSearchButton/CloseSearchButton.js
+++ src/components/SearchForm/CloseSearchButton/CloseSearchButton.js
@@ -1,4 +1,4 @@
-import ArrowLeftIcon from '../../../Icons/ArrowLeftIcon/ArrorLeftIcon'
+import ArrowLeftIcon from '../../Icons/ArrowLeftIcon/ArrowLeftIcon'
 import styles from './CloseSearchButton.module.css'
 
 function CloseSearchButton({ closeSearch }) {
