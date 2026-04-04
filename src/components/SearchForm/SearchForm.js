f55b99b537123b601e93872d7163e9099ad598e9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-04 16:58:21 -0400

    Update SearchForm.js

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -33,6 +33,9 @@ function SearchForm({ closeSearch }) {
   }, 600)
 
   const storeSearchTermLocally = value => {
+
+    console.log('config: ', config)
+    
     const key = config.storage.search.terms
     const item = localStorage.getItem(key)
     const existing = item ? JSON.parse(item) : []
