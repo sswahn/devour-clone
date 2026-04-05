409f1f62db6c4096982fb98d422c17716722d3d1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-05 12:47:58 -0400

    Update Notifications.js

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -1,6 +1,9 @@
 import styles from './Notifications.module.css'
 
 function Notifications({ closeNotifications }) {
+
+  // use bottomsheet
+  
   return (
     <section className={styles.notifications}>
     </section>
