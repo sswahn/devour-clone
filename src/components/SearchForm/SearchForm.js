1d38f73d786ac0237c487e333a04b8bddfe77f94
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-05 09:41:09 -0400

    Update SearchForm.js

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -89,7 +89,7 @@ function SearchForm({ closeSearch }) {
   
       {/* make Suggestions component: */}
       {loading ? <LoadingSpinner /> : (
-        <ul role="listbox">
+        <ul id="suggestions" role="listbox">
           {recentSearches?.length > 0 && recentSearches.map((search, index) =>
             <li key={index} role="option">{search}</li>
           )}    
