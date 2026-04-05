8a312689211677c26e9689d2f446883a0cbe5433
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-05 14:09:57 -0400

    Update Notifications.js

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react'
 // notifications arrive from server push events
 // the button must 'know' of updates so they will be set via context
 import styles from './Notifications.module.css'
@@ -6,10 +7,32 @@ function Notifications({ closeNotifications }) {
   const context = { 
     notifications: [1, 2, 3]
   }
+  const touchStartY = useRef()
+  const touchEndY = useRef()
   
-  // use bottomsheet
-  // .notifications class is the overlay, and contains the bottomsheet
-  // 
+  function handleGesture() {
+    if (touchEndY.current > touchStartY.current + 50) {
+      closeNotifications()
+    }
+  }
+
+  const touchStart = event => {
+    touchStartY.current = event.changedTouches[0].screenY
+  }
+  
+  const touchEnd = event => {
+    touchEndY.current = event.changedTouches[0].screenY
+    handleGesture()
+  }
+
+  useEffect(() => {
+    element.addEventListener('touchstart', touchStart)
+    element.addEventListener('touchend', touchEnd)
+    return () => {
+      element.removeEventListener('touchstart', touchStart)
+      element.removeEventListener('touchend', touchEnd)
+    }
+  }, [])
   
   return (
     <div className={styles.notifications}>
