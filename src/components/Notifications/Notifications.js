4e98c78192ff59a0e2c9283b891b2d5527e6fcfc
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-05 12:47:01 -0400

    Create Notifications.js

---
+++ src/components/Notifications/Notifications.js
@@ -0,0 +1,10 @@
+import styles from './Notifications.module.css'
+
+function Notifications({ closeNotifications }) {
+  return (
+    <section className={styles.notifications}>
+    </section>
+  )
+}
+
+export default Notifications
