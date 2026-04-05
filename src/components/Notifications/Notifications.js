13eb811d1e1389625430951fa958041c3432dd4d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-05 13:22:41 -0400

    Update Notifications.js

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -1,15 +1,24 @@
+// notifications arrive from server push events
+// the button must 'know' of updates so they will be set via context
 import styles from './Notifications.module.css'
 
 function Notifications({ closeNotifications }) {
-
+  const context = { 
+    notifications: [1, 2, 3]
+  }
+  
   // use bottomsheet
-
   // .notifications class is the overlay, and contains the bottomsheet
   // 
   
   return (
     <section className={styles.notifications}>
-      
+
+      <ul>
+        {context.notifications?.map(notification => 
+          <li>{notification}</li>                                                           
+        )}
+      </ul>
     </section>
   )
 }
