b7aa0ff138079ecfc3a25748eb6faf7d58ace83c
sswahn <sswahn@users.noreply.github.com>
2026-04-05 16:49:38 +0000

    deploy: f232a94ef065bcceae9730cfd59b6e20ee94cb67

---
+++ src_components_Notifications_Notifications_js.main.js
@@ -0,0 +1,32 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkdevour_clone"] = self["webpackChunkdevour_clone"] || []).push([["src_components_Notifications_Notifications_js"],{
+
+/***/ "./src/components/Notifications/Notifications.js"
+/*!*******************************************************!*\
+  !*** ./src/components/Notifications/Notifications.js ***!
+  \*******************************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _Notifications_module_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Notifications.module.css */ \"./src/components/Notifications/Notifications.module.css\");\n/* provided dependency */ var React = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nfunction Notifications(_ref) {\n  var closeNotifications = _ref.closeNotifications;\n  // use bottomsheet\n\n  return /*#__PURE__*/React.createElement(\"section\", {\n    className: _Notifications_module_css__WEBPACK_IMPORTED_MODULE_0__[\"default\"].notifications\n  });\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Notifications);\n\n//# sourceURL=webpack://devour-clone/./src/components/Notifications/Notifications.js?\n}");
+
+/***/ },
+
+/***/ "./src/components/Notifications/Notifications.module.css"
+/*!***************************************************************!*\
+  !*** ./src/components/Notifications/Notifications.module.css ***!
+  \***************************************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n// extracted by mini-css-extract-plugin\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({\"notifications\":\"EW8UjTxcaJyjDU58ZAQw\"});\n\n//# sourceURL=webpack://devour-clone/./src/components/Notifications/Notifications.module.css?\n}");
+
+/***/ }
+
+}]);
---
+++ src_components_Profile_Profile_js.main.js
@@ -0,0 +1,32 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkdevour_clone"] = self["webpackChunkdevour_clone"] || []).push([["src_components_Profile_Profile_js"],{
+
+/***/ "./src/components/Profile/Profile.js"
+/*!*******************************************!*\
+  !*** ./src/components/Profile/Profile.js ***!
+  \*******************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _Profile_module_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Profile.module.css */ \"./src/components/Profile/Profile.module.css\");\n/* provided dependency */ var React = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nfunction Profile(_ref) {\n  var closeProfile = _ref.closeProfile;\n  return /*#__PURE__*/React.createElement(\"section\", {\n    className: _Profile_module_css__WEBPACK_IMPORTED_MODULE_0__[\"default\"].profile\n  });\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Profile);\n\n//# sourceURL=webpack://devour-clone/./src/components/Profile/Profile.js?\n}");
+
+/***/ },
+
+/***/ "./src/components/Profile/Profile.module.css"
+/*!***************************************************!*\
+  !*** ./src/components/Profile/Profile.module.css ***!
+  \***************************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n// extracted by mini-css-extract-plugin\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({\"profile\":\"bRXXEOH49SRQPlqWmeUf\"});\n\n//# sourceURL=webpack://devour-clone/./src/components/Profile/Profile.module.css?\n}");
+
+/***/ }
+
+}]);
