0dfa48e5ef7a95790bacf137ba54da926de9bc6c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-07 12:35:28 -0400

    Update Notifications.js

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -53,6 +53,7 @@ function Notifications({ closeNotifications }) {
 
   const handlePointerDown = event => {
     if (listRef.current.scrollTop <= 0) {
+      listRef.current.style.pointerEvents = 'none'
       event.currentTarget.setPointerCapture(event.pointerId)
       dragging.current = true
       startY.current = event.clientY
