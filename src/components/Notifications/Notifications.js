8c07632c8b1b5a026040cd06470192b887ffa45d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-07 09:19:08 -0400

    Update Notifications.js

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -58,8 +58,7 @@ function Notifications({ closeNotifications }) {
     const list = listRef.current
     
     if (deltaY > 0 && list.scrollTop <= 0) {
-    //  list.style.overflow = 'hidden'
-      list.style.pointerEvents = 'none'
+      list.style.overflow = 'hidden'
     } else {
       return
     }
@@ -93,8 +92,7 @@ function Notifications({ closeNotifications }) {
     const bottomSheet = bottomSheetRef.current
     bottomSheet.style.transition = 'transform 100ms cubic-bezier(0.25, 1, 0.5, 1), height 100ms ease'
     bottomSheet.style.height = ''
-   // listRef.current.style.overflow = ''
-    listRef.current.style.pointerEvents = ''
+    listRef.current.style.overflow = ''
     if (deltaY > bottomSheet.offsetHeight / 2 || velocity > 0.5) {
       bottomSheet.style.transform = ''
       bottomSheet.addEventListener('transitionend', closeNotifications, { once: true })
