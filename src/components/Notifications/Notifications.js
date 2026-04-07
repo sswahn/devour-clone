44087e557225d80614e53b9126ce1b795e700599
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-07 10:59:07 -0400

    Update Notifications.js

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -55,7 +55,7 @@ function Notifications({ closeNotifications }) {
     dragging.current = false
     const bottomSheet = bottomSheetRef.current
     bottomSheet.style.transition = 'transform 100ms cubic-bezier(0.25, 1, 0.5, 1), height 100ms ease'
-    bottomSheet.style.transform = '' // 'translateY(0)'
+    bottomSheet.style.transform = 'translateY(0)'
     bottomSheet.style.height = ''
     listRef.current.style.overflow = ''
   }
