cf65c00a8b877fe67a441112516b1f0b0f750d3c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-08 12:59:54 -0400

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,16 +1,24 @@
+import { useRef, useEffect } from 'react'
 import styles from './Profile.module.css'
 
 function Profile({ closeProfile }) {
-
+  const sentinelStartRef = useRef(null)
+  const sentinelEndRef = useRef(null)
+  
   // use sentinel focus trap pattern
-  // use useEffect to load initial focus on load,
+  // use useEffect to load initial focus on load, (header or close btn)
 
+  const focusLast = event => {}
+  const focusFirst = event => {}
+  
   const handleClose = event => {
     closeProfile()
   }
   
+  
   return (
     <section className={styles.profile} role="dialog" aria-modal="true">
+      <div className="sentinel" ref={sentinelStartRef} onFocus={focusLast} tabIndex="0"></div>
       <header>
         <button onClick={handleClose} type="button">
           
@@ -20,6 +28,7 @@ function Profile({ closeProfile }) {
       <div>
         {/* feed */}
       </div>
+      <div className="sentinel" ref={sentinelEndRef} onFocus={focusFirst} tabIndex="0"></div>
     </section>
   )
 }
