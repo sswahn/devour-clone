b207c539534a0815b06c87b97cde65097dc5c040
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-08 18:49:14 -0400

    Update FollowBottom.js

--- src/components/Profile/FollowButton/FollowBottom.js
+++ src/components/Profile/FollowButton/FollowBottom.js
@@ -1,14 +1,20 @@
 import { useState } from 'react'
+import styles from 'FollowButton.module.css'
 
 function FollowButton() {
   use [following, setFollowing] = useState(false)
 
   const handleFollow = event => {
-    
+    setFollowing(prevState => !prevState)
   }
   
   return (
-    <button onClick={handleFollow} type="button" aria-label={`{following ? ${'unfollow'} : ${'follow'}}`} aria-pressed={following}>
+    <button 
+      className={styles.followButton} 
+      onClick={handleFollow} 
+      type="button" 
+      aria-label={`{following ? ${'unfollow'} : ${'follow'}}`} 
+      aria-pressed={following}>
       + Follow
     </button>
   )
