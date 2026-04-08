3d3e15c5ad63d84e1cc6e034bd3d4585e4e836ed
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-08 13:46:48 -0400

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,7 +5,7 @@ import styles from './Profile.module.css'
 function Profile({ closeProfile }) {
   const sentinelStartRef = useRef(null)
   const sentinelEndRef = useRef(null)
-  const closeButtonRef = useRef(null)
+  const headerRef = useRef(null)
   
   // use sentinel focus trap pattern
   // use useEffect to load initial focus on load, (header or close btn)
@@ -17,18 +17,19 @@ function Profile({ closeProfile }) {
   const handleUnFollow = event => {}
 
   useEffect(() => {
-    closeButtonRef.current.focus()
+    headerRef.current.focus()
   }, [])
   
   return (
-    <section className={styles.profile} role="dialog" aria-modal="true" labelledby="profile-username">
+    <section className={styles.profile} role="dialog" aria-modal="true" aria-labelledby="profile-username">
       <div className="sentinel" ref={sentinelStartRef} onFocus={focusLast} tabIndex="0" aria-hidden="true"></div>
-  
-      <CloseProfileButton closeProfile={closeProfile} />
-      <figure>
-        <img src="" alt="" />
-      </figure>
-      <h1 id="profile-username">Username</h1>
+      <header ref={headerRef}>
+        <CloseProfileButton closeProfile={closeProfile} />
+        <figure>
+          <img src="" alt={`{''}'s profile`} />
+        </figure>
+        <h1 id="profile-username">Username</h1>
+      </header>
         {/* user.bio && <p>{user.bio}</p> */}
       <address>Location</address>
       
