2f16729a798f77c2f9b2a393dc6423fd0f0fd97c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-08 15:47:38 -0400

    Update CameraButton.js

--- src/components/MobileUI/Navigation/CameraButton/CameraButton.js
+++ src/components/MobileUI/Navigation/CameraButton/CameraButton.js
@@ -6,13 +6,19 @@ function CameraButton({ cameraButtonRef, openCamera }) {
   const onClick = async event => {
     navigator.vibrate(50); return;
    
-    await document.getElementById('portal').requestFullscreen()
-    await screen.orientation.lock('portrait')
+    // await document.getElementById('portal').requestFullscreen()
+    // await screen.orientation.lock('portrait')
     openCamera()
   }
   
   return (
-    <button className={styles.cameraButton} ref={cameraButtonRef} onClick={onClick} type="button" aria-label="open camera" aria-haspopup="dialog">
+    <button 
+      className={styles.cameraButton} 
+      ref={cameraButtonRef} 
+      onClick={onClick} 
+      type="button" 
+      aria-label="open camera" 
+      aria-haspopup="dialog">
       <CameraIcon size={32} />  
     </button>
   )
