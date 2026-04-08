c086792f18f98e83ba849eb285a9911b7b6da745
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-08 13:36:07 -0400

    Update Avatar.js

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -7,7 +7,7 @@ const Avatar = ({ className, image, username, onClick, size }) => {
   return (
     <div className={`${styles.avatar} ${className || ''}`} onClick={onClick} aria-label={`${username}'s avatar`}>
       {image 
-        ? <img src={image} alt={`${username}'s avatar`} width={size || '24px'} height={size || '24px'} />
+        ? <img src={image} alt={`${username}'s avatar`} loading="lazy" width={size || '24px'} height={size || '24px'} />
         : <UserIcon size={size} />
       }
     </div>
