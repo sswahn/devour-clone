705f09aa623179bd3794721b7f8c04b57025fd28
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-08 12:21:45 -0400

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,26 @@
 import styles from './Profile.module.css'
 
 function Profile({ closeProfile }) {
+
+  const handleBlur = event => {
+    // Ensure relatedTarget exists and check if focus left the container
+    if (event.relatedTarget && !event.currentTarget.contains(event.relatedTarget)) {
+      // Focus has left the container; refocus a specific element
+      // actually instead of currentTarget, focus on the closeButton
+      event.currentTarget.focus()
+    }
+  }
+
+  const handleClose = event => {
+    closeProfile()
+  }
   
   return (
-    <section className={styles.profile} role="dialog" aria-modal="true">
+    <section className={styles.profile} onBlur={handleBlur} role="dialog" aria-modal="true">
       <header>
-      
+        <button onClick={handleClose} type="button">
+          
+        </button>
         {/* close button, profile image, username, location, and follow button */}
       </header>
       <div>
