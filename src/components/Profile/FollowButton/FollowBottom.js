5af851890f1f6e89650789d7474062adafe59a17
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-08 18:40:44 -0400

    Update FollowBottom.js

--- src/components/Profile/FollowButton/FollowBottom.js
+++ src/components/Profile/FollowButton/FollowBottom.js
@@ -1,7 +1,14 @@
+import { useState } from 'react'
+
 function FollowButton() {
+  use [following, setFollowing] = useState(false)
+
+  const handleFollow = event => {
+    
+  }
   
   return (
-    <button onClick={handleFollow} type="button">
+    <button onClick={handleFollow} type="button" aria-label={`{following ? ${'unfollow'} : ${'follow'}}`} aria-pressed={following}>
       + Follow
     </button>
   )
