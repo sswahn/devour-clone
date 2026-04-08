d74d0f7b454afa804031a88c57afe614e40d7d64
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-08 15:45:14 -0400

    Update ProfileButton.js

--- src/components/MobileUI/Navigation/ProfileButton/ProfileButton.js
+++ src/components/MobileUI/Navigation/ProfileButton/ProfileButton.js
@@ -1,7 +1,7 @@
 import UserIcon from '../../../Icons/UserIcon/UserIcon'
 import styles from './ProfileButton.module.css'
 
-function ProfileButton({ openProfile }) {
+function ProfileButton({ profileButtonRef, openProfile }) {
   
   const onClick = async event => {
     navigator.vibrate(50)
@@ -11,7 +11,7 @@ function ProfileButton({ openProfile }) {
   }
   
   return (
-    <button className={styles.profileButton} onClick={onClick} type="button" aria-label="open profile" aria-haspopup="dialog">
+    <button className={styles.profileButton} ref={profileButtonRef} onClick={onClick} type="button" aria-label="open profile" aria-haspopup="dialog">
       <UserIcon />  
     </button>
   )
