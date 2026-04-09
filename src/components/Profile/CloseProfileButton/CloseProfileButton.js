a9e0ab69b99b7a7613ab8ceb4d2b5faef4ca11c2
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-09 11:19:51 -0400

    Update CloseProfileButton.js

--- src/components/Profile/CloseProfileButton/CloseProfileButton.js
+++ src/components/Profile/CloseProfileButton/CloseProfileButton.js
@@ -1,9 +1,9 @@
 import ArrowLeftIcon from '../../Icons/ArrowLeftIcon/ArrowLeftIcon'
 import styles from './CloseProfileButton.module.css'
 
-function CloseProfileButton({ name, closeButtonRef, close }) {
+function CloseProfileButton({ name, close }) {
  return (
-   <button className={styles.closeButton} ref={closeButtonRef} onClick={close} type="button" aria-label={`close ${name}`}>
+   <button className={styles.closeButton} onClick={close} type="button" aria-label={`close ${name}`}>
      <ArrowLeftIcon />
    </button>
   )
