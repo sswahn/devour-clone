b13d6fd161b49e53b81452aa88876031193952e0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-09 15:29:32 -0400

    Update FocusTrap.js

--- src/components/MobileUI/FocusTrap.js
+++ src/components/MobileUI/FocusTrap.js
@@ -3,6 +3,18 @@ import { useRef } from 'react'
 function FocusTrap({ children }) {
   const startRef = useRef(null)
   const endRef = useRef(null)
+  const overlayRef = useRef(null)
+
+  const focusLast = event => {
+    console.log('first sentinel triggered!')
+    const { length } = profileRef.current.children
+    profileRef.current.children[length - 2].focus()
+  }
+  
+  const focusFirst = event => {
+    console.log('last sentinel triggered!')
+    profileRef.current.children[1].focus()
+  }
   
   return (
     <>
