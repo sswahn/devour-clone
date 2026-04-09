7532aa44fa9710bd8ed5f8bdb0c5dca70f1e2b1d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-09 17:15:21 -0400

    Update FocusTrapProvider.js

--- src/components/Providers/FocusTrapProvider.js
+++ src/components/Providers/FocusTrapProvider.js
@@ -7,7 +7,6 @@ function FocusTrapProvider({ children }) {
   
   const overlayRef = useCallback(node => {
     if (node !== null) {
-      console.log('ref in focustrap provider: ', node)
       node.focus()
       focusedRef.current = node
     } else { 
@@ -17,21 +16,11 @@ function FocusTrapProvider({ children }) {
   }, [])
 
   const focusLast = event => {
-    
-    console.log('first sentinel triggered!')
-    
-    console.log('overlayRef: ', focusedRef)
-    
     const { length } = focusedRef.children
     focusedRef.current.children[length - 1].focus()
   }
   
   const focusFirst = event => {
-    
-    console.log('last sentinel triggered!')
-
-    console.log('overlayRef: ', focusedRef)
-    
     focusedRef.current.children[0].focus()
   }
     
