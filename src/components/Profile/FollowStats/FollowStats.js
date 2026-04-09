3b4e085851c252783b436e086421dafb4e8782a3
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-09 11:09:42 -0400

    Update FollowStats.js

--- src/components/Profile/FollowStats/FollowStats.js
+++ src/components/Profile/FollowStats/FollowStats.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react'
 
 function FollowStats() {
+  const [following, setFollowing] = useState(600)
+  const [followers, setFollowers] = useState(50)
+
+  const handleShowFollowing = event => {}
+  const handleShowFollowers = event => {}
+  
+  // get live websocket updates on counts
+  
   return (
-    <ul></ul>
+    <ul>
+      <li>
+        <button onClick={handleShowFollowing} type="button"><strong>{following}</strong> Following</button>
+      </li>
+      <li>
+        <button onClick={handleShowFollowers} type="button"><strong>{followers}</strong> Followers</button>
+      </li>
+    </ul>
   )
 }
 
