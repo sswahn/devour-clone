ad71a222e6ca435895372b508784790736e6a4e6
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 10:01:05 -0400

    Update FollowingButton.js

--- src/components/Profile/FollowStats/FollowingButton.js
+++ src/components/Profile/FollowStats/FollowingButton.js
@@ -1,9 +1,14 @@
+import { useState } from 'react'
 import styles from './FollowingButton.module.css'
 
 function FollowingButton({ username, count }) {
-
+  const [viewFollowing, setViewFollowing] = useState(false)
+  
   const action = () => {
+    setViewFollowing(prevState => !prevState)
     // request users being followed by user
+    // request should be in parent so list populates
+    // just need function call here: requestFollowing()
   }
   
   const onClick = event => {
@@ -22,8 +27,9 @@ function FollowingButton({ username, count }) {
       onKeyDown={onKeyDown} 
       type="button" 
       aria-label={`view users followed by ${username}`}
-      >
-      <strong>{count}</strong> Following
+      aria-pressed={viewFollowing}
+      aria-controls="profile-feed">
+      <strong>{count}</strong> <span>Following</span>
     </button>
   )
 }
