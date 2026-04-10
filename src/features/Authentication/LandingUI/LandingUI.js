fbc8ef024845a7ba2af09f4e57b78f1886d1038b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 14:25:38 -0400

    Update LandingUI.js

--- src/features/Authentication/LandingUI/LandingUI.js
+++ src/features/Authentication/LandingUI/LandingUI.js
@@ -1,13 +1,14 @@
-import styles from './LandingUI.module.css'
+import LoginButton from './LoginButton/LoginButton'
+import styles from './LoginUI.module.css'
 
-function LandingUI() {
+function LoginUI() {
   return (
-    <section className={styles.landingUI}>
-      <button className={styles} onClick={onClick} onKeyDown={onKeyDown} type="text" aria-label="sign in">Sign In</button>
-      <button className={styles} onClick={onClick} onKeyDown={onKeyDown} type="text" aria-label="sign up">Sign Up</button>
+    <section className={styles.loginUI}>
+      <LoginButton text="Sign In" action={setOpenLogin} aria={{label: 'sign in'}} />
+      <LoginButton text="Sign Up" action={setOpenRegistration} aria={{label: 'sign up'}} />
       <hr />
-      <button className={styles} onClick={onClick} onKeyDown={onKeyDown} type="text" aria-label="sign in with Google">Sign in with Google</button>
-      <button className={styles} onClick={onClick} onKeyDown={onKeyDown} type="text" aria-label="sign in with Apple">Sign in with Apple</button>
+      <LoginButton text="Continue with Google" action="" aria={{label: 'continue with Google'}} />
+      <LoginButton text="Continue with Apple" action="" aria={{label: 'continue with Apple'}} />
     </section>    
   )
 }
