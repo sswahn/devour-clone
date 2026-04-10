210a32dfdccdc9e06c5db3e8b7a6c6bfe2c0f666
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 14:59:21 -0400

    Create Root.js

---
+++ src/components/Root/Root.js
@@ -0,0 +1,22 @@
+import { useContext } from 'react'
+// useContext or
+// useSession custom hook
+/*
+function Root() {
+
+  return (
+    <SessionProvider>
+      {session 
+      ?
+        <Authentication /> 
+      : 
+        <>
+          <Header />
+          <Main />
+          <MobileUI />
+        <>
+      }
+    </SessionProvider>
+  )
+}
+  */
