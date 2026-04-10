646b0235a8350c56a2dc6207dced48a4b7c5cd47
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 12:03:12 -0400

    Update SessionProvider.js

--- src/components/Providers/SessionProvider.js
+++ src/components/Providers/SessionProvider.js
@@ -1,1 +1,14 @@
-import { createContext } from 'react'
+import { useReducer, createContext } from 'react'
+
+const SessionContext = createContext()
+
+function SessionProvider({ children }) {
+  
+  return (
+    <SessionContext.Provider>
+      {children}
+    </SessionContext.Provider>
+  )
+}
+
+export { SessionContext, SessionProvider }
