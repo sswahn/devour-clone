c15dc4a5c10d9bcc03696e7a1e1bfd87bf91e78a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 13:24:43 -0400

    Update Checkbox.js

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -6,15 +6,30 @@ import styles from './Checkbox.module.css'
 const Checkbox = memo(({ label, checked, onChange }) => {
   const checkboxRef = useRef(null)
 
-  const handleClick = event => {
+  const action = () => {
     checkboxRef.current.click()
   }
+
+  const onClick = event => {
+    action()
+  }
+
+  const onKeyDown = event => {
+    if (event.key === 'Enter') {
+      action()
+    }
+  }
+
+  // the icons should be conditionally rendered in a button
+  // it should receive the onClick and onKeyDown methods as props
   
   return (
-    <button className={styles.checkbox} onClick={handleClick}>
-      <label>{label}</label>
+    <button className={styles.checkbox}>
+      <label onClick={onClick} onKeyDown={onKeyDown}>{label}</label>
       <input ref={checkboxRef} type="checkbox" checked={checked} onChange={onChange} />
+  
       {checked ? <CheckedIcon /> : <UncheckedIcon />}
+  
     </button>
   )
 })
