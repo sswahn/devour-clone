bb3d2650bc7ac206e1007f382ceae400d461cc7d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 10:09:21 -0400

    Update FollowersButton.js

--- src/components/Profile/FollowStats/FollowersButton/FollowersButton.js
+++ src/components/Profile/FollowStats/FollowersButton/FollowersButton.js
@@ -1,7 +1,30 @@
+import styles from './FollowersButton.module.css'
 
-function FollowersButton() {
+function FollowersButton({ username, count }) {
+
+  const action = () => {
+    
+  }
+  
+  const onClick = event => {
+    action()
+  }
+  
+  const onKeyPress = event => {
+    if (event.key === 'Enter') {
+      action()
+    }
+  }
+  
   return (
-    <></>
+    <button
+      className={styles.followersButton}
+      onClick={onClick} 
+      onKeyDown={onKeyDown}
+      type="button"
+      aria-label="">
+      <strong>{count}</strong> <span>Followers</span>
+    </button>
   )
 }
 
