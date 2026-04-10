9466da951da820413d3d0394a17b30f1ed62f90c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 13:39:38 -0400

    Update Login.js

--- src/features/Authentication/Login/Login.js
+++ src/features/Authentication/Login/Login.js
@@ -1,29 +1,37 @@
+import { setState } from 'react'
 import Checkbox from '../../../components/Checkbox/Checkbox'
 import LoadingSpinner from '../../../components/LoadingSpinner/LoadingSpinner'
 import SubmitButton from './SubmitButton/SubmitButton'
 import styles from './Login.module.css'
 
 function Login() {
-
+  const [checked, setChecked] = useState(false)
   
+  const handleCheckbox = event => {
+    setChecked(prevState => !prevState)
+  }
+
+  // buttons should go to their own componenets
+  const forgotPassword = event => {}
+  const registerUser = event => {}
   
   return (
     <form className={styles.login} onSubmit={onSubmit} aria-label="login form">
       <h2>Sign In</h2>
       <div>
-        <input id="username" type="text" onChange={handleOnChange} placeholder="Username" required minLength={2} maxLength={50} autoComplete="off" aria-label="username" />
+        <input id="username" type="text" placeholder="Username" required minLength={2} maxLength={50} autoComplete="off" aria-label="username" />
         <UserIcon />
       </div>
       <div>
-        <input id="password" type="password" onChange={handleOnChange} placeholder="Password" required minLength={8} maxLength={130} autoComplete="off" aria-label="password" />
+        <input id="password" type="password" placeholder="Password" required minLength={8} maxLength={130} autoComplete="off" aria-label="password" />
         <PasswordIcon />
       </div>
       <Checkbox label="Remember me" checked={checked} onChange={handleCheckbox} />
       {loading && <LoadingSpinner />}
       {!loading && <SubmitButton text="Sign In" message={message} error={error} disabled={!!message} />}
       <div>
-        <button type="button" onClick={forgotPassword} aria-label="open forgot password form">Forgot password?</button>
-        <button type="button" onClick={registerUser} aria-label="open registration form">Create an account</button>
+        <button onClick={forgotPassword} type="button" aria-label="open forgot password form">Forgot password?</button>
+        <button onClick={registerUser} type="button" aria-label="open registration form">Create an account</button>
       </div>
     </form>
   )
