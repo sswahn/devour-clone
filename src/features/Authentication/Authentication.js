4398cb40dfe54acefa33dbfe2a0180000e91ccf8
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 14:37:09 -0400

    Update Authentication.js

--- src/features/Authentication/Authentication.js
+++ src/features/Authentication/Authentication.js
@@ -1,9 +1,17 @@
+import { useState } from 'react'
+// import LoginUI from './LoginUI/LoginUI'
+// import LoginForm from './LoginForm/LoginForm'
+// import RegistrationForm from './RegistrationForm/RegistrationForm'
 
 function Authentication() {
-
+  const [openLogin, setOpenLogin] = useState(false)
+  const [openRegistration, setOpenRegistration] = useState(false)
+  
   // all primary render logic goes here
   return (
-    
+    {openLogin && <LoginForm /> }
+    {openRegistration && <RegistrationForm />}
+    {(!openLogin && !openRegistration) && <LoginUI />}
   )
 }
 
