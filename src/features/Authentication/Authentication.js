e0cc457140cfeb7c3c79be3aa6df8f174097ab52
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 14:47:45 -0400

    Update Authentication.js

--- src/features/Authentication/Authentication.js
+++ src/features/Authentication/Authentication.js
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import { useState, Suspense, lazy } from 'react'
 import AuthUI from './AuthUI/AuthUI'
-// import LoginForm from './LoginForm/LoginForm'
-// import RegistrationForm from './RegistrationForm/RegistrationForm'
+const LoginForm = lazy(() => import('./LoginForm/LoginForm'))
+const RegistrationForm = lazy(() => import('./RegistrationForm/RegistrationForm'))
 
 function Authentication() {
   const [openLogin, setOpenLogin] = useState(false)
   const [openRegistration, setOpenRegistration] = useState(false)
-  
-  // all primary render logic goes here
+
   return (
-    {(!openLogin && !openRegistration) && <AuthUI />}
+    <>
+      {(!openLogin && !openRegistration) && <AuthUI />}
+      <Suspense fallback={<LoadingSpinner />}>
+        {openLogin && <LoginForm closeSearch={closeSearch} />}
+      </Suspense>
+      <Suspense fallback={<LoadingSpinner />}>
+        {openRegistration && <RegistrationForm />} />}
+      </Suspense>
+    </>
   )
 }
 
