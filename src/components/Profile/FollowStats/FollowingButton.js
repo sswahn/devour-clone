32555276c0b68fca8966740c344e019986fc3189
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 09:50:24 -0400

    Update FollowingButton.js

--- src/components/Profile/FollowStats/FollowingButton.js
+++ src/components/Profile/FollowStats/FollowingButton.js
@@ -1,7 +1,23 @@
 
-function FollowingButton() {
+function FollowingButton({ following }) {
+
+  const action = () => {
+    // request users being followed by user
+  }
+  
+  const onClick = event => {
+    action()
+  }
+  
+  const onKeyDown = event => {
+    if (event.key === 'Enter') {
+      action()
+    }
+  }
   return (
-    <></>
+    <button onClick={onClick} onKeyDown={onKeyDown} type="button" aria-label="view following">
+      <strong>{following}</strong> Following
+    </button>
   )
 }
 
