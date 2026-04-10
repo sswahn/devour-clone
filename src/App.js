e4c072f3a3c6c361372819f7005b8e0a579f0c76
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 15:11:41 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import ErrorBoundary from './ErrorBoundary'
-import Header from './components/Header/Header'
-import Main from './components/Main/Main'
-import MobileUI from './components/MobileUI/MobileUI'
+import SessionProvider from './components/Provider/SessionProvider'
+import AppRoot from './components/AppRoot/AppRoot'
 import './index.css'
 
 function App() {
   return (
     <React.StrictMode> 
       <ErrorBoundary> 
-        <Header />
-        <Main />
-        <MobileUI />
+        <SessionProvider>
+          <AppRoot />
+        </SessionProvider>
       </ErrorBoundary> 
     </React.StrictMode>
   )
