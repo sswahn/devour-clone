67b684bd17179887b397dda96e37ae2d07043991
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-10 14:02:23 -0400

    Update SubmitButton.js

--- src/features/Authentication/Login/SubmitButton/SubmitButton.js
+++ src/features/Authentication/Login/SubmitButton/SubmitButton.js
@@ -5,20 +5,23 @@ import styles from './styles.module.css'
 
 const SubmitButton = memo(({ text, message, error, disabled }) => {
   return (
-    <div>
+    <div> 
+    {/* login shouldnt have success message, just open a loading screen while feed loads, no need to message success */}
+    {/* registration can have success message, but it shouldnt be part of button component. */}
       {message && (
         <div className={styles.success}>
           <CheckIcon />
           <span>{message}</span>
         </div>
       )}
+   {/* move error message outside of button component */}
       {error && (
         <div className={styles.error}>
           <ExclamationIcon />
           <span>{error}</span>
         </div>
       )}
-      <button className={styles.submit} type="submit" disabled={disabled}>{text}</button>
+      <button className={styles.submit} type="submit" disabled={disabled} aria-label="">{text}</button>
     </div>
   )
 })
