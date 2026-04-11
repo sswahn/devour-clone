87efd6582aefac9debb3a8285fd8ce2b4850bafb
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-11 11:41:21 -0400

    Update AuthUI.js

--- src/features/Authentication/AuthUI/AuthUI.js
+++ src/features/Authentication/AuthUI/AuthUI.js
@@ -3,7 +3,6 @@ import SignInButton from './SignInButton/SignInButton'
 import SignUpButton from './SignUpButton/SignUpButton'
 import GoogleButton from './GoogleButton/GoogleButton'
 import AppleButton from './AppleButton/AppleButton'
-import Appl from './SignIn/SignIn'
 import styles from './AuthUI.module.css'
 
 function AuthUI() {
