725dc17ffd7dca807a154ae6fd39b1f2cceb7db0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-12 18:22:54 -0400

    Update ConfirmForgotPassword.js

--- src/features/Authentication/ForgotPassword/ConfirmForgotPassword.js
+++ src/features/Authentication/ForgotPassword/ConfirmForgotPassword.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import OpenEmailIcon from './OpenEmailIcon.js'
+import EnvelopeOpenIcon from '../../../components/Icons/EnvelopeOpenIcon/EnvelopeOpenIcon'
 import UserIcon from '../../../components/Icons/UserIcon/UserIcon'
 import LockIcon from '../../../components/Icons/LockIcon/LockIcon'
 import styles from './ForgotPassword.module.css'
@@ -76,7 +76,7 @@ function ConfirmForgotPassword() {
       <h2>Create a new password</h2>
       <div>
         <input id="confirmation_code" type="text" onChange={handleOnChange} placeholder="Confirmation code." required minLength={1} maxLength={255} autoComplete="off" aria-label="confirmation code" />
-        <OpenEmailIcon />
+        <EnvelopeOpenIcon />
       </div>
       <div>
         <input id="username" type="text" onChange={handleOnChange} placeholder="Username" required minLength={2} maxLength={50} pattern="^(?!_)[a-zA-Z0-9_]{1,48}(?<!_)$" autoComplete="off" aria-label="username" />
@@ -84,11 +84,11 @@ function ConfirmForgotPassword() {
       </div>
       <div>
         <input id="password" type="password" onChange={handleOnChange} placeholder="Password" required minLength={8} maxLength={130} pattern="^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z]).{8,}$" autoComplete="off" aria-label="password" />
-        <PasswordIcon />
+        <LockIcon />
       </div>
       <div>
         <input id="confirm_password" type="password" onChange={handleOnChange} required minLength={8} maxLength={130} placeholder="Confirm Password" autoComplete="off" aria-label="confirm password" />
-        <PasswordIcon />
+        <LockIcon />
       </div>
       <button type="submit" aria-label="confirm reset password">Confirm Reset Password</button>}
     </form>
