bc0f7b60dab897fb053d7150db40fcce77ab0342
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-12 16:24:02 -0400

    Update Registration.js

--- src/features/Authentication/Registration/Registration.js
+++ src/features/Authentication/Registration/Registration.js
@@ -80,15 +80,15 @@ const Registration = ({ closeRegistration }) => {
       </div>
       <div>
         <input id="email" type="email" onChange={handleOnChange} placeholder="Email" required minLength={7} maxLength={254} autoComplete="off" aria-label="email" />
-        <EmailIcon />
+        <EnvelopeIcon />
       </div>
       <div>
         <input id="password" type="password" onChange={handleOnChange} placeholder="Password" required minLength={8} maxLength={130} pattern="^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z]).{8,}$" autoComplete="off" aria-label="password" />
-        <PasswordIcon />
+        <LockIcon />
       </div>
       <div>
         <input id="confirmPassword" type="password" onChange={handleOnChange} required minLength={8} maxLength={130} placeholder="Confirm Password" autoComplete="off" aria-label="confirm password" />
-        <PasswordIcon />
+        <LockIcon />
       </div>
       <details aria-label="password requirements">
         <summary aria-haspopup="true">Password requirements</summary>
