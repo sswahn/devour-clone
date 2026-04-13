fbdfc1f7ce9f6e6700deb9ad160f334fb44c00ba
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-13 14:39:45 -0400

    Update Login.js

--- src/features/Authentication/Login/Login.js
+++ src/features/Authentication/Login/Login.js
@@ -1,13 +1,17 @@
 import Logo from '../../components/Logo/Logo'
 import LoginForm from './LoginForm'
-import FederatedLoginButtons from './FederatedLoginButtons'
+import GoogleButton from './GoogleButton/GoogleButton'
+import AppleButton from './AppleButton/AppleButton'
 
 function Login() {
   return (
     <section>
       <Logo size={40} />
       <LoginForm />
-      <FederatedLoginButtons />
+      <div>
+        <GoogleButton />
+        <AppleButton />
+      </div>
     </section>
   )
 }
