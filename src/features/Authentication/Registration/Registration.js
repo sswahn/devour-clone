48b07725742f3dc90cb5b39dd5dab7902a8c13ea
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-13 16:53:52 -0400

    Update Registration.js

--- src/features/Authentication/Registration/Registration.js
+++ src/features/Authentication/Registration/Registration.js
@@ -1,10 +1,12 @@
 import styles from './Registration.module.css'
 import Logo from '../../../components/Logo/Logo'
+import RegistrationForm from './RegistrationForm/RegistrationForm'
 
 function Registration() {
   return (
     <section className={styles.registration}>
       <Logo size={40} />
+      <RegistrationForm />
     </section>
   )
 }
