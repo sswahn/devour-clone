fe5e4b53463320dab103d9586df2c559f7589e67
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-13 16:41:36 -0400

    Update Login.js

--- src/features/Authentication/Login/Login.js
+++ src/features/Authentication/Login/Login.js
@@ -4,7 +4,7 @@ import GoogleButton from './GoogleButton/GoogleButton'
 import AppleButton from './AppleButton/AppleButton'
 import styles from './Login.module.css'
 
-function Login() {
+function Login({ openForgotPassword, openRegistration }) {
   return (
     <section className={styles.login}>
       <Logo size={40} />
