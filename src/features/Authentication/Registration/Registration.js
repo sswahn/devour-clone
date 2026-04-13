a89b2666bb1b28b984a4e2a67b5739e2d38e7adf
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-13 16:55:10 -0400

    Update Registration.js

--- src/features/Authentication/Registration/Registration.js
+++ src/features/Authentication/Registration/Registration.js
@@ -2,9 +2,10 @@ import styles from './Registration.module.css'
 import Logo from '../../../components/Logo/Logo'
 import RegistrationForm from './RegistrationForm/RegistrationForm'
 
-function Registration() {
+function Registration({ closeRegistration }) {
   return (
     <section className={styles.registration}>
+      <CloseButton text="registration form" close={closeRegistration} />
       <Logo size={40} />
       <RegistrationForm />
     </section>
