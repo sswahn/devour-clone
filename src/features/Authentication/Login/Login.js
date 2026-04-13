956fc56c976dc938ca5e7d865a1a814c459487a0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-13 14:34:05 -0400

    Update Login.js

--- src/features/Authentication/Login/Login.js
+++ src/features/Authentication/Login/Login.js
@@ -1,10 +1,11 @@
-
+import Logo from '../../components/Logo/Logo'
 import LoginForm from './LoginForm'
 import FederatedLoginButtons from './FederatedLoginButtons'
 
 function Login() {
   return (
     <section>
+      <Logo size={40} />
       <LoginForm />
       <FederatedLoginButtons />
     </section>
