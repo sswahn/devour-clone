497d6e6788a88ba42c6a03b2eb38706659755927
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-13 14:29:44 -0400

    Create Login.js

---
+++ src/features/Authentication/Login/Login.js
@@ -0,0 +1,14 @@
+
+import LoginForm from './LoginForm'
+import FederatedLoginButtons from './FederatedLoginButtons'
+
+function Login() {
+  return (
+    <section>
+      <LoginForm />
+      <FederatedLoginButtons />
+    </section>
+  )
+}
+
+export default Login
