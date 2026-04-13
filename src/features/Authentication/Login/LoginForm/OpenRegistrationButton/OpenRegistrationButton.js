ab34bbeb51be5a773b996c2b1ff7a794e891a1c9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-13 15:00:26 -0400

    Update OpenRegistrationButton.js

--- src/features/Authentication/Login/LoginForm/OpenRegistrationButton/OpenRegistrationButton.js
+++ src/features/Authentication/Login/LoginForm/OpenRegistrationButton/OpenRegistrationButton.js
@@ -1,8 +1,29 @@
 import styles from './OpenRegistrationButton.module.css'
 
-function OpenRegistrationButton() {
+function OpenRegistrationButton({ openRegistration }) {
+
+  const action = () => {
+    openRegistration()
+  }
+  
+  const onClick = event => {
+    action()
+  }
+  
+  const onKeyDown = event => {
+    if (event.key === 'Enter') {
+      action()
+    }
+  }
   return (
-    <></>
+    <button 
+      className={styles.openRegistrationButton} 
+      onClick={onClick} 
+      onKeyDown={onKeyDown} 
+      type="button" 
+      aria-label="open registration form">
+      Create an account
+    </button>
   )
 }
 
