bf28bec7df9e3f43c9152cf880b976867e08bb0c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-13 16:52:24 -0400

    Create Registration.js

---
+++ src/features/Authentication/Registration/Registration.js
@@ -0,0 +1,11 @@
+import styles from './Registration.module.css'
+
+function Registration() {
+  return (
+    <section className={styles.registration}>
+    
+    </section>
+  )
+}
+
+export default Registration
