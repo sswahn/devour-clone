91cf86054755809c4310b920799da7baa04e6f3c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-14 09:19:47 -0400

    Update LoginForm.js

--- src/features/Authentication/Login/LoginForm/LoginForm.js
+++ src/features/Authentication/Login/LoginForm/LoginForm.js
@@ -17,9 +17,9 @@ function LoginForm() {
   
   return (
     <form className={styles.loginForm} onSubmit={onSubmit} aria-label="login form">
-      <label htmlFor="email">Email:</label>
-      <input id="email" type="text" required maxLength={50} autoComplete="username webauthn" aria-label="email" />
-      <button type="submit" aria-label="sign in">Continue</button>
+      <label htmlFor="email">Email or Username:</label>
+      <input id="username" type="text" required maxLength={50} autoComplete="username webauthn" aria-label="email or username" />
+      <button type="submit" aria-label="continue signing in">Continue</button>
     </form>
   )
 }
