c17afd0d23bc41b088a749a5cf6429f67a991da7
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-14 09:03:51 -0400

    Update LoginForm.js

--- src/features/Authentication/Login/LoginForm/LoginForm.js
+++ src/features/Authentication/Login/LoginForm/LoginForm.js
@@ -1,13 +1,8 @@
 import { useState, useContext } from 'react'
 import { SetSessionContext } from '../../../../components/Providers/SessionProvider'
-import UserIcon from '../../../../components/Icons/UserIcon/UserIcon'
-import LockIcon from '../../../../components/Icons/LockIcon/LockIcon'
-import Checkbox from '../../../../components/Checkbox/Checkbox'
-import ForgotPasswordButton from './ForgotPasswordButton/ForgotPasswordButton'
-import OpenRegistrationButton from './OpenRegistrationButton/OpenRegistrationButton'
 import styles from './LoginForm.module.css'
 
-function LoginForm({ openForgotPassword, openRegistration }) {
+function LoginForm() {
   const setSession = useContext(SetSessionContext) // onSubmit, setSession({isAuthenticated: true, ...user_data})
   const [checked, setChecked] = useState(false)
   const [loading, setLoading] = useState(false)
@@ -22,20 +17,9 @@ function LoginForm({ openForgotPassword, openRegistration }) {
   
   return (
     <form className={styles.loginForm} onSubmit={onSubmit} aria-label="login form">
-      <div>
-        <input id="username" type="text" placeholder="Username" required maxLength={50} autoComplete="username" aria-label="username" />
-        <UserIcon size={20} />
-      </div>
-      <div>
-        <input id="password" type="password" placeholder="Password" required maxLength={130} autoComplete="current-password" aria-label="password" />
-        <LockIcon size={20} />
-      </div>
-      <Checkbox label="Remember me" checked={checked} onChange={handleCheckbox} />
-      <button type="submit" aria-label="sign in">Sign In</button>
-      <div>
-        <ForgotPasswordButton openForgotPassword={openForgotPassword} />
-        <OpenRegistrationButton openRegistration={openRegistration} />
-      </div>
+      <label htmlFor="email">Email:</label>
+      <input id="email" type="text" required maxLength={50} autoComplete="email" aria-label="email" />
+      <button type="submit" aria-label="sign in">Continue</button>
     </form>
   )
 }
