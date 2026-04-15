25ef0e333677d2fabfa67844524bfceaa0ca2d15
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-15 16:44:53 -0400

    Remove Logo component from Authentication

--- src/features/Authentication/Authentication.js
+++ src/features/Authentication/Authentication.js
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import Logo from '../../components/Logo/Logo'
 import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner'
 import LoginForm from './LoginForm/LoginForm'
 import RegistrationButton from './RegistrationButton/RegistrationButton'
@@ -10,7 +9,6 @@ import styles from './Authentication.module.css'
 function Authentication() {
   return (
     <section className={styles.authentication}>
-      <Logo size={40} />
       <LoginForm />
       <RegistrationButton />
       <GoogleButton />
