b6e861b9b064fa2b4caa06a59c51c2e6169efce0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-15 16:35:47 -0400

    Update LoginForm.js

--- src/features/Authentication/LoginForm/LoginForm.js
+++ src/features/Authentication/LoginForm/LoginForm.js
@@ -6,6 +6,9 @@ function LoginForm() {
   const setSession = useContext(SetSessionContext) // onSubmit, setSession({isAuthenticated: true, ...user_data})
   const [loading, setLoading] = useState(false)
 
+  // Perform validation checks in javascript and return alert if violated.
+  // ex. username.length > 50 characters, etc. -> error out.
+
   const onSubmit = event => {
     event.preventDefault()
     // initiate auth call to backend
@@ -18,7 +21,7 @@ function LoginForm() {
   return (
     <form className={styles.loginForm} onSubmit={onSubmit} aria-label="login form">
       <label htmlFor="username">Email or username:</label>
-      <input id="username" type="text" required maxLength={50} autoComplete="username webauthn" aria-label="email or username" />
+      <input id="username" type="text" required autoComplete="username webauthn" aria-label="email or username" />
       <button type="submit" aria-label="sign in">Sign In</button>
     </form>
   )
