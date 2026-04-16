0993ac1c5e34e89b62f4e2ead75a25225e1e33f0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-16 10:41:28 -0400

    Update DropdownButton.js

--- src/components/DropdownButton/DropdownButton.js
+++ src/components/DropdownButton/DropdownButton.js
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import EllipsisVerticalIcon from '../Icons/EllipsisVerticalIcon/EllipsisVerticalIcon'
 import styles from './DropdownButton.module.css'
 
-function DropdownButton({ id = 0, data }) {
+function DropdownButton({ id = 0, label = 'dropdown', items }) {
   const [isOpen, setIsOpen] = useState(false)
+  const listRef = useRef(null)
   
   const action = () => {
     setIsOpen(prevState => !prevState)
+    listRef.current.children[0].focus() 
   }
   
   const onClick = event => {
@@ -18,6 +20,11 @@ function DropdownButton({ id = 0, data }) {
       action()
     }
   }
+
+  // break list item button into its own component
+  // same with main button, 
+  // same with <ul> itself.
+  // combine in <Dropdown /> component
   
   return (
     <>
@@ -27,14 +34,16 @@ function DropdownButton({ id = 0, data }) {
         onClick={onClick} 
         onKeyDown={onKeyDown} 
         type="button" 
-        aria-label="open dropdown" 
+        aria-label={label} 
         aria-haspopup="menu" 
         aria-expanded={isOpen} 
         aria-controls={`dropdown-list-${id}`}>
         <EllipsisVerticalIcon />
       </button>
-      <ul id={`dropdown-list-${id}`} role="menu" aria-labelledby={`dropdown-button-${id}`} hidden>
-        {data?.map((item, index) => <li key={index} role="menuitem">{item}</li>}
+      <ul id={`dropdown-list-${id}`} ref={listRef} role="menu" aria-labelledby={`dropdown-button-${id}`} hidden={!isOpen}>
+        {items?.map((item, index) => <li key={index} role="menuitem">
+          <button onClick={item.action} onKeyDown={item.action} type="button">{item.text}</button>
+        </li>}
       </ul>
     </>
   )
