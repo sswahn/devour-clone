01d22f62c1bf081f51189882bb4567b7bc6240c1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-16 14:33:13 -0400

    Refactor focus functions for button navigation

--- src/components/Dropdown/ListItemButton/ListItemButton.js
+++ src/components/Dropdown/ListItemButton/ListItemButton.js
@@ -2,14 +2,14 @@ import styles from './ListItemButton.module.css'
 
 function ListItemButton({ buttonRef, listRef, text, method, close }) {
   
-  const focusPrevButton = target => {
-    const button = target.parentElement.previousElementSibling.firstElementChild
-    button ? button.focus() : listRef.current.lastElementChild.firstElementChild.focus()
+  const focusPrev = button => {
+    const prevButton = button.parentElement.previousElementSibling.firstElementChild
+    prevButton ? prevButton.focus() : listRef.current.lastElementChild.firstElementChild.focus()
   }
 
-  const focusNextButton = target => {
-    const button = target.parentElement.nextElementSibling.firstElementChild
-    button ? button.focus() : listRef.current.firstElementChild.firstElementChild.focus()
+  const focusNext = button => {
+    const nextButton = button.parentElement.nextElementSibling.firstElementChild
+    nextButton ? nextButton.focus() : listRef.current.firstElementChild.firstElementChild.focus()
   }
 
   const onClick = event => {
@@ -27,10 +27,10 @@ function ListItemButton({ buttonRef, listRef, text, method, close }) {
         return buttonRef.current?.focus() // 2. Return focus to button on close
       case 'ArrowDown':
         event.preventDefault() // test with and without this (supposedly it keeps the screen from scrolling)
-        return focusNextButton(event.target)
+        return focusNext(event.target)
       case 'ArrowUp':
         event.preventDefault() // test with and without this (supposedly it keeps the screen from scrolling)
-        return focusPrevButton(event.target)
+        return focusPrev(event.target)
       default:
         return
     }
