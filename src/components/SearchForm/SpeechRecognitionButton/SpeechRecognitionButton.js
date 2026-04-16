578d86b6d1ba0eaddc604b8e5be4ac69f19200c6
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-16 14:49:46 -0400

    Update SpeechRecognitionButton.js

--- src/components/SearchForm/SpeechRecognitionButton/SpeechRecognitionButton.js
+++ src/components/SearchForm/SpeechRecognitionButton/SpeechRecognitionButton.js
@@ -55,7 +55,7 @@ function SpeechRecognitionButton({ setSearchValue }) {
       type="button" 
       aria-label="speech recognition"
       aria-description="search using your voice">
-      <MicrophoneIcon size={12} />
+      <MicrophoneIcon size={16} />
     </button>
   )
 }
