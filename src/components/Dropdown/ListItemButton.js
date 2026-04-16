68e28054ef26e30ecfeecceb36dcd46c4ef6dd57
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-16 13:56:59 -0400

    Create ListItemButton.js

---
+++ src/components/Dropdown/ListItemButton.js
@@ -0,0 +1,11 @@
+
+function ListItemButton() {
+  
+  
+  
+  return (
+    <button onClick={item.method} onKeyDown={item.method} type="button" role="menuitem" tabIndex="-1">{item.text}</button>
+  )
+}
+
+export default ListItemButton
