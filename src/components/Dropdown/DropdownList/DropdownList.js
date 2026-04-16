4ee0b0c7f6b46c2a35e022b1fbdbf32e0bc312a9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-16 13:50:14 -0400

    Update DropdownList.js

--- src/components/Dropdown/DropdownList/DropdownList.js
+++ src/components/Dropdown/DropdownList/DropdownList.js
@@ -1,10 +1,23 @@
+import { useEffect } from  'react'
 import styles from './DropdownList.module.css'
 
-function DropdownList() {
+function DropdownList({ isOpen, items }) {
 
   
   return (
-    <ul></ul>
+    <ul 
+      id={`dropdown-list-${id}`} 
+      className={styles.dropdownList} 
+      ref={listRef} 
+      role="menu" 
+      aria-labelledby={`dropdown-button-${id}`} 
+      hidden={!isOpen}>
+      {items?.map((item, index) => 
+        <li key={index} role="none">
+          <button onClick={item.method} onKeyDown={item.method} type="button" role="menuitem" tabIndex="-1">{item.text}</button>
+        </li>
+      )}
+    </ul>
   )
 }
 
