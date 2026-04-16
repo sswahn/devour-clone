9b3e5b78a57ecc45d2ffd19b76394b18d09925e3
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-16 14:10:15 -0400

    Refactor Dropdown component to use DropdownList

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -5,7 +5,6 @@ import styles from './Dropdown.module.css'
 function Dropdown({ id = 0, label = 'dropdown', items }) {
   const [isOpen, setIsOpen] = useState(false)
   const buttonRef = useRef(null)
-  const listRef = useRef(null)
   
   const action = () => {
     setIsOpen(prevState => !prevState)
@@ -21,41 +20,8 @@ function Dropdown({ id = 0, label = 'dropdown', items }) {
     }
   }
 
-  const focusPrevButton = target => {
-    const button = target.parentElement.previousElementSibling.firstElementChild
-    button ? button.focus() : listRef.current.lastElementChild.firstElementChild.focus()
-  }
-
-  const focusNextButton = target => {
-    const button = target.parentElement.nextElementSibling.firstElementChild
-    button ? button.focus() : listRef.current.firstElementChild.firstElementChild.focus()
-  }
-
-  // move this to the ul component
-  const onKeyDownForListButtons = event => {
-    switch (event.key) {
-      case 'Enter':
-        return action()
-        
-      case 'ArrowDown':
-        event.preventDefault() // test with and without this (supposedly it keeps the screen from scrolling)
-        return focusNextButton(event.target)
-
-      case 'ArrowUp':
-        event.preventDefault() // test with and without this (supposedly it keeps the screen from scrolling)
-        return focusPrevButton(event.target)
-
-      case 'Escape':
-        close()
-        buttonRef.current?.focus() // 2. Return focus to button on close
-        return
-
-      case 'Tab':
-        return close()  // Standard behavior: Close menu if user tabs out
-
-      default:
-        return
-    }
+  const close = () => {
+    setIsOpen(false)
   }
 
   useEffect(() => {
@@ -78,13 +44,8 @@ function Dropdown({ id = 0, label = 'dropdown', items }) {
         aria-controls={`dropdown-list-${id}`}>
         <EllipsisVerticalIcon />
       </button>
-      <ul id={`dropdown-list-${id}`} ref={listRef} role="menu" aria-labelledby={`dropdown-button-${id}`} hidden={!isOpen}>
-        {items?.map((item, index) => 
-          <li key={index} role="none">
-            <button onClick={item.method} onKeyDown={item.method} type="button" role="menuitem" tabIndex="-1">{item.text}</button>
-          </li>
-        )}
-      </ul>
+      
+      <DropdownList id={id} isOpen={isOpen} close={close} items={items} />
     </div>
   )
 }
