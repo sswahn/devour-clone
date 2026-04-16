bcc2d72a6d793bbe430be038b7d87baca0649d94
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-16 14:21:04 -0400

    Update Dropdown.js

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,5 +1,4 @@
 import { useState, useRef, useEffect } from 'react'
-import EllipsisVerticalIcon from '../Icons/EllipsisVerticalIcon/EllipsisVerticalIcon'
 import styles from './Dropdown.module.css'
 
 function Dropdown({ id = 0, label = 'dropdown', items }) {
@@ -12,20 +11,8 @@ function Dropdown({ id = 0, label = 'dropdown', items }) {
 
   return (
     <div　className={styles.dropdown}>
-      <button 
-        id={`dropdown-button-${id}`}
-        ref={buttonRef}
-        onClick={onClick} 
-        onKeyDown={onKeyDown} 
-        type="button" 
-        aria-label={label} 
-        aria-haspopup="menu" 
-        aria-expanded={isOpen} 
-        aria-controls={`dropdown-list-${id}`}>
-        <EllipsisVerticalIcon />
-      </button>
       <DropdownButton id={id} label={label} isOpen={isOpen} setIsOpen={setIsOpen} buttonRef={buttonRef} /> 
-      <DropdownList id={id} isOpen={isOpen} close={close} items={items} />
+      <DropdownList id={id} isOpen={isOpen} close={close} items={items} buttonRef={buttonRef} />
     </div>
   )
 }
