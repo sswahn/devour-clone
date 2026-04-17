d039facc25097fe43e078b5e4710dc89106786e9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-17 13:36:24 -0400

    Update useFocusTrap.js

--- src/hooks/useFocusTrap.js
+++ src/hooks/useFocusTrap.js
@@ -1,8 +1,13 @@
+import { useContext } from 'react'
+import { FocusTrapContext } from '../Providers/FocusTrapProvider'
 
 function useFocusTrap() {
+  const focusRef = useContext(FocusTrapContext)
+  
   if (!focusRef) {
     // error
   }
+  
   return focusRef
 }
 
