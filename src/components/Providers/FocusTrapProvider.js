5b9842cbbdbc84581bddd7204dc1318057d014c1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-17 15:15:06 -0400

    Update FocusTrapProvider.js

--- src/components/Providers/FocusTrapProvider.js
+++ src/components/Providers/FocusTrapProvider.js
@@ -21,27 +21,12 @@ function FocusTrapProvider({ children }) {
 
   const focusLast = event => {
     const elements = ref.current.querySelectorAll(selector)
-    const { length } = elements
-    
-    console.log('focusLast: ', elements)
-    console.log('elements.length: ', length)
-    console.log('elements[length - 1]: ', elements[length - 1])
-    
-    if (length > 0) {
-      elements[length - 1].focus()
-    }
+    elements[elements.length - 1]?.focus()
   }
   
   const focusFirst = event => {
     const elements = ref.current.querySelectorAll(selector)
-    
-    console.log('focusFirst: ', elements)
-    console.log('elements.length: ', elements.length)
-    console.log('elements[0]: ', elements[0])
-    
-    if (elements.length > 0) {
-      elements[0].focus()
-    }
+    elements[0]?.focus()
   }
     
   return (
