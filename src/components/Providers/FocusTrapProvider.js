1ac0cabc3641e15102c3767486d0f6bd816622a8
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-17 13:51:06 -0400

    Update FocusTrapProvider.js

--- src/components/Providers/FocusTrapProvider.js
+++ src/components/Providers/FocusTrapProvider.js
@@ -1,39 +1,41 @@
-import { useRef, useCallback, createContext } from 'react'
+import { useState, useRef, useCallback, createContext } from 'react'
 
 const FocusTrapContext = createContext(null)
 
 function FocusTrapProvider({ children }) {
+  cont [isMounted, setIsMounted] = useState(false)
   const ref = useRef(null)
   
   const focusRef = useCallback(node => {
     if (node !== null) {
       ref.current = node
       ref.current.focus()
+      setIsMounted(true)
     } else {  
-      console.log('focusRef cleanup fired. focusRef.current = null')
       ref.current = null // 2. Cleanup logic (unmount)
+      setIsMounted(false)
     }
   }, [])
 
   const focusLast = event => {
     console.log('focusLast fired!')
-    console.log('focusing on focusRef.current.lastElementChild: ', focusRef.current.lastElementChild) 
+    console.log('focusing on ref.current.lastElementChild: ', ref.current.lastElementChild) 
     
-    focusRef.current.lastElementChild.focus()
+    ref.current.lastElementChild.focus()
   }
   
   const focusFirst = event => {
     console.log('focusFirst fired!')
-    console.log('focusing on focusRef.current.firstElementChild: ', focusRef.current.firstElementChild)
+    console.log('focusing on ref.current.firstElementChild: ', ref.current.firstElementChild)
     
-    focusRef.current.firstElementChild.focus()
+    ref.current.firstElementChild.focus()
   }
     
   return (
     <FocusTrapContext.Provider value={focusRef}>
-      {focusRef.current && <div onFocus={focusLast} tabIndex={0}></div>}
+      {isMounted && <div onFocus={focusLast} tabIndex={0}></div>}
         {children}
-      {focusRef.current && <div onFocus={focusFirst} tabIndex={0}></div>}
+      {isMounted && <div onFocus={focusFirst} tabIndex={0}></div>}
     </FocusTrapContext.Provider>
   )
 }
