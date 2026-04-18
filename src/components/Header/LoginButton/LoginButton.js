892a15d6a24e51924fee5575eee4fd7926b00fbe
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-18 15:43:30 -0400

    Update LoginButton.js

--- src/components/Header/LoginButton/LoginButton.js
+++ src/components/Header/LoginButton/LoginButton.js
@@ -1,8 +1,12 @@
 
 
 function LoginButton() {
+
+  
   return (
-    
+    <button onClick={onClick} onKeyDown={onKeyDown} ref={authenticationButtonRef} type="button" aria-label="sign in">
+      <RightToBracketIcon />
+    </button>
   )
 }
 
