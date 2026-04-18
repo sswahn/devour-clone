d55471312092bdc6cd70812ab13e6538f18e03c0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-18 15:44:00 -0400

    Update LoginButton.js

--- src/components/Header/LoginButton/LoginButton.js
+++ src/components/Header/LoginButton/LoginButton.js
@@ -2,6 +2,23 @@
 
 function LoginButton() {
 
+  const action = () => {
+    console.log('openAuth fired!')
+    
+    navigator.vibrate(50)
+    openAuthentication()  
+  }
+  
+  const onClick = event => {
+    action()
+  }
+  
+  const onKeyDown = event => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      action()
+    }
+  }
   
   return (
     <button onClick={onClick} onKeyDown={onKeyDown} ref={authenticationButtonRef} type="button" aria-label="sign in">
