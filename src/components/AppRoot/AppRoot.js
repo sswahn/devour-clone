b024a667f5ac4a3a5a90ee5d54e25483fed0e3d0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-18 10:38:20 -0400

    Update AppRoot.js

--- src/components/AppRoot/AppRoot.js
+++ src/components/AppRoot/AppRoot.js
@@ -3,7 +3,7 @@ import { GetSessionContext } from '../Providers/SessionProvider'
 import Authentication from '../../features/Authentication/Authentication'
 import Header from '../Header/Header'
 import Main from '../Main/Main'
-import MobileUI from '../MobileUI/MobileUI'
+import Interface from '../Interface/Interface'
 
 function AppRoot() {
   const session = useContext(GetSessionContext)
@@ -13,7 +13,7 @@ function AppRoot() {
       {session.isAuthenticated ? <Authentication /> : <>
         <Header />
         <Main />
-        <MobileUI />
+        <Interface />
       </>}
     </>
   )
