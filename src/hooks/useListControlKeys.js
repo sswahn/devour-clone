3be263651c1b659c1ad777e2f70101a4bf0128a1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-18 08:30:11 -0400

    Update useListControlKeys.js

--- src/hooks/useListControlKeys.js
+++ src/hooks/useListControlKeys.js
@@ -1,9 +1,47 @@
+import { useEffect } from 'react'
 
+function useListControlKeys(list, methods) {
+  const focusPrev = button => {
+    button === list.firstElementChild.firstElementChild
+      ? list.lastElementChild.firstElementChild.focus()
+      : button.parentElement.previousElementSibling.firstElementChild.focus()
+  }
 
-function useListControlKeys() {
-  return (
-    <></>
-  )
+  const focusNext = button => {
+    button === list.lastElementChild.firstElementChild
+      ? list.firstElementChild.firstElementChild.focus() 
+      : button.parentElement.nextElementSibling.firstElementChild.focus()
+  }
+
+  const onKeyDown = event => {
+    // event.stopPropagation() <-- consider placing here.
+    switch (event.key) {
+      case 'Enter':
+        event.preventDefault()
+        return methods.enter()
+      case 'Tab':
+        return close()  // Standard behavior: Close menu if user tabs out
+      case 'Escape':
+        event.stopPropagation()
+        event.preventDefault()
+        return methods.escape() // Return focus to button on close
+      case 'ArrowDown':
+        event.preventDefault()
+        return focusNext(event.target)
+      case 'ArrowUp':
+        event.preventDefault()
+        return focusPrev(event.target)
+      default:
+        return
+    }
+  }
+
+  useEffect(() => {
+    list.addEventListener('keydown', onKeyDown)
+    return () => {
+      list.removeEventListener('keydown', onKeyDown)
+    }
+  }, [])
 }
 
 export default useListControlKeys
