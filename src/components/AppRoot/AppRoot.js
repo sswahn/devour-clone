359470e1f8d874deb9317fb60e3acb837c7ba4dd
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-18 11:09:01 -0400

    Update AppRoot.js

--- src/components/AppRoot/AppRoot.js
+++ src/components/AppRoot/AppRoot.js
@@ -3,6 +3,11 @@ import { GetSessionContext } from '../Providers/SessionProvider'
 import Authentication from '../../features/Authentication/Authentication'
 import Interface from '../Interface/Interface'
 
+// not keeping this file
+// Interface loads at the app level
+// Login button on header
+// and camera has a condition, if not logged-in open login form
+
 function AppRoot() {
   const session = useContext(GetSessionContext)
   return session.isAuthenticated ? <Authentication /> : <Interface />
