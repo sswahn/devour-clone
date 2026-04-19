0439f19c943afd23a43167e59224fc2c5e242bf1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-19 12:14:54 -0400

    Update Avatar.js

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -4,12 +4,23 @@ import styles from './styles.module.css'
 
 const Avatar = ({ username, image, size = 24 }) => {
 
-  const onClick = event => {
+  const action = () => {
     // open usernames profile
   }
   
+  const onClick = event => {
+    action()
+  }
+
+  const onKeyDown = event => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      action()
+    }
+  }
+  
   return (
-    <div className={styles.avatar} onClick={onClick} aria-label={`${username}'s avatar`}>
+    <div className={styles.avatar} onClick={onClick} onKeyDown={onKeyDown} aria-label={`${username}'s avatar`}>
       {image 
         ? <img src={image} alt={`${username}'s avatar`} loading="lazy" width={size} height={size} />
         : <Identicon size={size} />
