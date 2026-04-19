4b9bab518e144d0e3b6165b3109d85f02a9eb366
S. Swahn <36422765+sswahn@users.noreply.github.com>
2026-04-19 13:29:58 -0400

    Create useProfile.js

---
+++ src/hooks/useProfile.js
@@ -0,0 +1,12 @@
+
+import { useContext } from 'react'
+import { GetProfileContext, SetProfileContext } from '../components/Providers/ProfileProvider'
+
+function useProfile() {
+  const username = useContext(GetProfileContext)
+  const setUsername = useContext(SetProfileContext)
+  
+  return { username, setUsername }
+}
+
+export default useProfile
